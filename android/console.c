aosp-mirror/platform_external_qemu

console: terminate connection on certain bad requests.
This patch ensures that the console terminates a connection when
it detects that an HTTP request was entered as a command, or that
the command contains an invalid UTF-8 sequence.

BUG=17584992

Change-Id: Iadbad75101211263d7deff73caf685a2481d8820

android/console.c
@@ -35,7 +35,9 @@
 #include "android/utils/bufprint.h"
 #include "android/utils/debug.h"
 #include "android/utils/eintr_wrapper.h"
+#include "android/utils/http_utils.h"
 #include "android/utils/stralloc.h"
+#include "android/utils/utf8_utils.h"
 #include "android/config/config.h"
 #include "android/tcpdump.h"
 #include "net/net.h"
@@ -403,6 +405,8 @@ dump_help( ControlClient  client,
     }
 }
 
+static int do_quit(ControlClient client, char* args);  // forward
+
 static void
 control_client_do_command( ControlClient  client )
 {
@@ -413,7 +417,16 @@ control_client_do_command( ControlClient  client )
     CommandDef  cmd      = find_command( line, commands, &cmdend, &args );
 
     if (cmd == NULL) {
-        control_write( client, "KO: unknown command, try 'help'\r\n" );
+        size_t line_len = strlen(line);
+        if (android_http_is_request_line(line, line_len)) {
+            control_write( client, "KO: Forbidden HTTP request. Aborting\r\n");
+            do_quit(client, NULL);
+        } else  if (!android_utf8_is_valid(line, line_len)) {
+            control_write( client, "KO: Forbidden binary request. Abortingr\r\n");
+            do_quit(client, NULL);
+        } else {
+            control_write( client, "KO: unknown command, try 'help'\r\n" );
+        }
         return;
     }
 

vicamo/libandroid-emugl

This is a back-port of https://android-review.googlesource.com/#/c/108762/
to our milestone branch.
	console: terminate connection on certain bad requests.
	This patch ensures that the console terminates a connection when
	it detects that an HTTP request was entered as a command, or that
	the command contains an invalid UTF-8 sequence.
Change-Id: Ib50f003415cedf1bfc20cc50703f736c5def0461
(cherry picked from commit de2bc2a3a3d4342d91893841bf202116236c7643)

android/console.c
@@ -35,7 +35,9 @@
 #include "android/utils/bufprint.h"
 #include "android/utils/debug.h"
 #include "android/utils/eintr_wrapper.h"
+#include "android/utils/http_utils.h"
 #include "android/utils/stralloc.h"
+#include "android/utils/utf8_utils.h"
 #include "android/config/config.h"
 #include "android/tcpdump.h"
 #include "net/net.h"
@@ -403,6 +405,8 @@ dump_help( ControlClient  client,
     }
 }
 
+static int do_quit(ControlClient client, char* args);  // forward
+
 static void
 control_client_do_command( ControlClient  client )
 {
@@ -413,7 +417,16 @@ control_client_do_command( ControlClient  client )
     CommandDef  cmd      = find_command( line, commands, &cmdend, &args );
 
     if (cmd == NULL) {
-        control_write( client, "KO: unknown command, try 'help'\r\n" );
+        size_t line_len = strlen(line);
+        if (android_http_is_request_line(line, line_len)) {
+            control_write( client, "KO: Forbidden HTTP request. Aborting\r\n");
+            do_quit(client, NULL);
+        } else  if (!android_utf8_is_valid(line, line_len)) {
+            control_write( client, "KO: Forbidden binary request. Aborting\r\n");
+            do_quit(client, NULL);
+        } else {
+            control_write( client, "KO: unknown command, try 'help'\r\n" );
+        }
         return;
     }
 
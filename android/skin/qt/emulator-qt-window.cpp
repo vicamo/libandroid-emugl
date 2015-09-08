aosp-mirror/platform_external_qemu

Zoom, rotate left, and quit implementations for Qt UI.
Hooks up the quit, zoom, and rotate left buttons in the Qt tools
window to have effect on the underlying emulator window.

Zoom currently toggles between two scales until a more permanent UI
solution (such as a dialog) is determined.

Change-Id: Icf153d72ed0c2961ccbde4e6ff3bad236a45f4ea

android/skin/qt/emulator-qt-window.cpp
@@ -480,14 +480,26 @@ void EmulatorQtWindow::handleKeyEvent(SkinEventType type, QKeyEvent *pEvent)
 
 void EmulatorQtWindow::simulateKeyPress(int keyCode, int modifiers)
 {
-    SkinEvent *event = new SkinEvent();
-    event->type = kEventKeyDown;
+    SkinEvent *event = createSkinEvent(kEventKeyDown);
     event->u.key.keycode = keyCode;
     event->u.key.mod = modifiers;
     slot_queueEvent(event);
-    event = new SkinEvent();
-    event->type = kEventKeyUp;
+
+    event = createSkinEvent(kEventKeyUp);
     event->u.key.keycode = keyCode;
     event->u.key.mod = modifiers;
+    slot_queueEvent(event);
+}
+
+void EmulatorQtWindow::simulateQuit()
+{
+    SkinEvent *event = createSkinEvent(kEventQuit);
+    slot_queueEvent(event);
+}
+
+void EmulatorQtWindow::simulateResize(float scale)
+{
+    SkinEvent *event = createSkinEvent(kEventResize);
+    event->u.resize.scale = scale;
     slot_queueEvent(event);
 }

android/skin/qt/emulator-qt-window.h
@@ -97,6 +97,8 @@ signals:
 
 public:
     void simulateKeyPress(int keyCode, int modifiers);
+    void simulateQuit();
+    void simulateResize(float scale);
 
 private slots:
     void slot_blit(QImage *src, QRect *srcRect, QImage *dst, QPoint *dstPos, QPainter::CompositionMode *op, QSemaphore *semaphore = NULL);
@@ -135,7 +137,6 @@ public slots:
     void slot_screenshot();
     void slot_up();
     void slot_voice();
-
     void slot_zoom();
 
 private:

vicamo/libandroid-emugl

(cherry picked from commit aaa7a8b37bb26d45592dd463b34f7e906448e3c4)

android/skin/qt/emulator-qt-window.cpp
@@ -486,14 +486,26 @@ void EmulatorQtWindow::handleKeyEvent(SkinEventType type, QKeyEvent *pEvent)
 
 void EmulatorQtWindow::simulateKeyPress(int keyCode, int modifiers)
 {
-    SkinEvent *event = new SkinEvent();
-    event->type = kEventKeyDown;
+    SkinEvent *event = createSkinEvent(kEventKeyDown);
     event->u.key.keycode = keyCode;
     event->u.key.mod = modifiers;
     slot_queueEvent(event);
-    event = new SkinEvent();
-    event->type = kEventKeyUp;
+
+    event = createSkinEvent(kEventKeyUp);
     event->u.key.keycode = keyCode;
     event->u.key.mod = modifiers;
+    slot_queueEvent(event);
+}
+
+void EmulatorQtWindow::simulateQuit()
+{
+    SkinEvent *event = createSkinEvent(kEventQuit);
+    slot_queueEvent(event);
+}
+
+void EmulatorQtWindow::simulateSetScale(double scale)
+{
+    SkinEvent *event = createSkinEvent(kEventSetScale);
+    event->u.resize.scale = scale;
     slot_queueEvent(event);
 }

android/skin/qt/emulator-qt-window.h
@@ -99,6 +99,8 @@ signals:
 
 public:
     void simulateKeyPress(int keyCode, int modifiers);
+    void simulateQuit();
+    void simulateSetScale(double scale);
 
 private slots:
     void slot_blit(QImage *src, QRect *srcRect, QImage *dst, QPoint *dstPos, QPainter::CompositionMode *op, QSemaphore *semaphore = NULL);
@@ -137,7 +139,6 @@ public slots:
     void slot_screenshot();
     void slot_up();
     void slot_voice();
-
     void slot_zoom();
 
 private:
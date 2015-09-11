aosp-mirror/platform_external_qemu

Add "battery" functionality
Add handlers for the GUI controls on the battery pane.
Implement the interface to the AVD for battery functions.

Change-Id: I227b30bf7dba2593d3a9ef2691179283576007ef

android/qemu-battery-agent-impl.c
@@ -1,0 +1,57 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/battery-agent.h"
+#include "android/battery-agent-impl.h"
+#include "hw/power_supply.h"
+#include "hw/android/goldfish/device.h"
+
+void battery_setIsCharging(int isCharging) {
+    goldfish_battery_set_prop(1, POWER_SUPPLY_PROP_ONLINE, isCharging);
+}
+
+void battery_setChargeLevel(int percentFull) {
+    goldfish_battery_set_prop(0, POWER_SUPPLY_PROP_CAPACITY, percentFull);
+}
+
+void battery_setHealth(enum BatteryHealth health) {
+    int value;
+
+    switch (health) {
+        case Battery_H_Good:          value = POWER_SUPPLY_HEALTH_GOOD;            break;
+        case Battery_H_Failed:        value = POWER_SUPPLY_HEALTH_UNSPEC_FAILURE;  break;
+        case Battery_H_Dead:          value = POWER_SUPPLY_HEALTH_DEAD;            break;
+        case Battery_H_Overvoltage:   value = POWER_SUPPLY_HEALTH_OVERVOLTAGE;     break;
+        case Battery_H_Overheated:    value = POWER_SUPPLY_HEALTH_OVERHEAT;        break;
+        case Battery_H_Unknown:       value = POWER_SUPPLY_HEALTH_UNKNOWN;         break;
+
+        default:                      value = POWER_SUPPLY_HEALTH_UNKNOWN;         break;
+    }
+
+    goldfish_battery_set_prop(0, POWER_SUPPLY_PROP_HEALTH, value);
+}
+
+void battery_setStatus(enum BatteryStatus status) {
+    int value;
+
+    switch (status) {
+        case Battery_S_Unknown:       value = POWER_SUPPLY_STATUS_UNKNOWN;       break;
+        case Battery_S_Charging:      value = POWER_SUPPLY_STATUS_CHARGING;      break;
+        case Battery_S_Discharging:   value = POWER_SUPPLY_STATUS_DISCHARGING;   break;
+        case Battery_S_Not_Charging:  value = POWER_SUPPLY_STATUS_NOT_CHARGING;  break;
+        case Battery_S_Full:          value = POWER_SUPPLY_STATUS_FULL;          break;
+
+        default:                      value = POWER_SUPPLY_STATUS_UNKNOWN;       break;
+    }
+
+    goldfish_battery_set_prop(0, POWER_SUPPLY_PROP_STATUS, value);
+}

android/skin/qt/ext-battery.cpp
@@ -1,0 +1,61 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include <QtWidgets>
+
+#include "android/battery-agent.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+void ExtendedWindow::initBattery()
+{ }
+
+void ExtendedWindow::on_bat_chargeCkBox_toggled(bool checked)
+{
+    batteryState.isCharging = checked;
+    if (batteryAgent && batteryAgent->setIsCharging) {
+        batteryAgent->setIsCharging(checked);
+    }
+}
+
+void ExtendedWindow::on_bat_levelSlider_valueChanged(int value)
+{
+    batteryState.chargeLevel = value;
+    if (batteryAgent && batteryAgent->setChargeLevel) {
+        batteryAgent->setChargeLevel(value);
+    }
+}
+
+void ExtendedWindow::on_bat_healthBox_currentIndexChanged(int index)
+{
+    BatteryHealth bHealth = (BatteryHealth)index;
+    if (bHealth >= 0 && bHealth < Battery_H_NUM_ENTRIES) {
+        batteryState.health = bHealth;
+
+        if (batteryAgent && batteryAgent->setHealth) {
+            batteryAgent->setHealth(bHealth);
+        }
+    }
+}
+
+void ExtendedWindow::on_bat_statusBox_currentIndexChanged(int index)
+{
+    BatteryStatus bStatus = (BatteryStatus)index;
+    if (bStatus >= 0 && bStatus < Battery_S_NUM_ENTRIES) {
+        batteryState.status = bStatus;
+
+        if (batteryAgent && batteryAgent->setStatus) {
+                batteryAgent->setStatus(bStatus);
+        }
+    }
+}

android/skin/qt/extended-window.cpp
@@ -51,6 +51,7 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     QFrame(eW),
     parentWindow(eW),
     toolWindow(tW),
+    batteryAgent  (agentPtr ? agentPtr->battery   : NULL),
     extendedUi(new Ui::ExtendedControls)
 {
     Q_INIT_RESOURCE(resources);
@@ -59,6 +60,9 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     setAttribute(Qt::WA_DeleteOnClose);
 
     extendedUi->setupUi(this);
+
+    // Do any sub-window-specific initialization
+    initBattery();
 
     move(parentWindow->geometry().right() + 40,
          parentWindow->geometry().top()   + 40 );

vicamo/libandroid-emugl

android/qemu-battery-agent-impl.c
@@ -1,0 +1,57 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/battery-agent.h"
+#include "android/battery-agent-impl.h"
+#include "hw/power_supply.h"
+#include "hw/android/goldfish/device.h"
+
+void battery_setIsCharging(bool isCharging) {
+    goldfish_battery_set_prop(1, POWER_SUPPLY_PROP_ONLINE, isCharging);
+}
+
+void battery_setChargeLevel(int percentFull) {
+    goldfish_battery_set_prop(0, POWER_SUPPLY_PROP_CAPACITY, percentFull);
+}
+
+void battery_setHealth(enum BatteryHealth health) {
+    int value;
+
+    switch (health) {
+        case BATTERY_HEALTH_GOOD:          value = POWER_SUPPLY_HEALTH_GOOD;            break;
+        case BATTERY_HEALTH_FAILED:        value = POWER_SUPPLY_HEALTH_UNSPEC_FAILURE;  break;
+        case BATTERY_HEALTH_DEAD:          value = POWER_SUPPLY_HEALTH_DEAD;            break;
+        case BATTERY_HEALTH_OVERVOLTAGE:   value = POWER_SUPPLY_HEALTH_OVERVOLTAGE;     break;
+        case BATTERY_HEALTH_OVERHEATED:    value = POWER_SUPPLY_HEALTH_OVERHEAT;        break;
+        case BATTERY_HEALTH_UNKNOWN:       value = POWER_SUPPLY_HEALTH_UNKNOWN;         break;
+
+        default:                           value = POWER_SUPPLY_HEALTH_UNKNOWN;         break;
+    }
+
+    goldfish_battery_set_prop(0, POWER_SUPPLY_PROP_HEALTH, value);
+}
+
+void battery_setStatus(enum BatteryStatus status) {
+    int value;
+
+    switch (status) {
+        case BATTERY_STATUS_UNKNOWN:       value = POWER_SUPPLY_STATUS_UNKNOWN;       break;
+        case BATTERY_STATUS_CHARGING:      value = POWER_SUPPLY_STATUS_CHARGING;      break;
+        case BATTERY_STATUS_DISCHARGING:   value = POWER_SUPPLY_STATUS_DISCHARGING;   break;
+        case BATTERY_STATUS_NOT_CHARGING:  value = POWER_SUPPLY_STATUS_NOT_CHARGING;  break;
+        case BATTERY_STATUS_FULL:          value = POWER_SUPPLY_STATUS_FULL;          break;
+
+        default:                           value = POWER_SUPPLY_STATUS_UNKNOWN;       break;
+    }
+
+    goldfish_battery_set_prop(0, POWER_SUPPLY_PROP_STATUS, value);
+}

android/skin/qt/ext-battery.cpp
@@ -1,0 +1,61 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include <QtWidgets>
+
+#include "android/battery-agent.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+void ExtendedWindow::initBattery()
+{ }
+
+void ExtendedWindow::on_bat_chargeCkBox_toggled(bool checked)
+{
+    batteryState.isCharging = checked;
+    if (batteryAgent && batteryAgent->setIsCharging) {
+        batteryAgent->setIsCharging(checked);
+    }
+}
+
+void ExtendedWindow::on_bat_levelSlider_valueChanged(int value)
+{
+    batteryState.chargeLevel = value;
+    if (batteryAgent && batteryAgent->setChargeLevel) {
+        batteryAgent->setChargeLevel(value);
+    }
+}
+
+void ExtendedWindow::on_bat_healthBox_currentIndexChanged(int index)
+{
+    BatteryHealth bHealth = (BatteryHealth)index;
+    if (bHealth >= 0 && bHealth < BATTERY_HEALTH_NUM_ENTRIES) {
+        batteryState.health = bHealth;
+
+        if (batteryAgent && batteryAgent->setHealth) {
+            batteryAgent->setHealth(bHealth);
+        }
+    }
+}
+
+void ExtendedWindow::on_bat_statusBox_currentIndexChanged(int index)
+{
+    BatteryStatus bStatus = (BatteryStatus)index;
+    if (bStatus >= 0 && bStatus < BATTERY_STATUS_NUM_ENTRIES) {
+        batteryState.status = bStatus;
+
+        if (batteryAgent && batteryAgent->setStatus) {
+                batteryAgent->setStatus(bStatus);
+        }
+    }
+}

android/skin/qt/extended-window.cpp
@@ -24,6 +24,7 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     QFrame(eW),
     mParentWindow(eW),
     mToolWindow(tW),
+    mBatteryAgent  (agentPtr ? agentPtr->battery   : NULL),
     mExtendedUi(new Ui::ExtendedControls)
 {
     Q_INIT_RESOURCE(resources);
@@ -32,6 +33,9 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     setAttribute(Qt::WA_DeleteOnClose);
 
     mExtendedUi->setupUi(this);
+
+    // Do any sub-window-specific initialization
+    initBattery();
 
     move(mParentWindow->geometry().right() + 40,
          mParentWindow->geometry().top()   + 40 );
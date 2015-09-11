aosp-mirror/platform_external_qemu

Add "fingerprint" function
Change-Id: I699dacac99ce02bfd7c0a2902deea78784810d77

android/skin/qt/ext-finger.cpp
@@ -1,0 +1,42 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/finger-agent.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+#include <QtWidgets>
+
+void ExtendedWindow::initFinger()
+{ }
+
+void ExtendedWindow::on_finger_touchCkBox_toggled(bool checked)
+{
+    bool OK = false;
+    int  id = extendedUi->finger_IdBox->toPlainText().toInt(&OK);
+
+    if (checked && !OK) {
+        QErrorMessage *eM = new QErrorMessage;
+        eM->showMessage(QString("The \"Fingerprint ID\" number is invalid"));
+        extendedUi->finger_IdBox->setEnabled(true);
+        extendedUi->finger_touchCkBox->setChecked(false);
+        return;
+    }
+
+    if (fingerAgent && fingerAgent->setTouch) {
+        fingerAgent->setTouch(checked, id);
+    }
+
+    // Don't allow changing the ID while a touch is active
+    extendedUi->finger_IdBox->setEnabled( !checked );
+}

android/skin/qt/extended-window.cpp
@@ -53,6 +53,7 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     toolWindow(tW),
     batteryAgent  (agentPtr ? agentPtr->battery   : NULL),
     cellularAgent (agentPtr ? agentPtr->cellular  : NULL),
+    fingerAgent   (agentPtr ? agentPtr->finger    : NULL),
     telephonyAgent(agentPtr ? agentPtr->telephony : NULL),
     extendedUi(new Ui::ExtendedControls)
 {
@@ -66,6 +67,7 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     // Do any sub-window-specific initialization
     initBattery();
     initCellular();
+    initFinger();
     initTelephony();
 
     move(parentWindow->geometry().right() + 40,

android/skin/qt/extended-window.h
@@ -25,6 +25,7 @@
 
 #include "android/battery-agent.h"
 #include "android/cellular-agent.h"
+#include "android/finger-agent.h"
 #include "android/telephony-agent.h"
 
 class EmulatorQtWindow;
@@ -82,6 +83,7 @@ private:
 
     void initBattery();
     void initCellular();
+    void initFinger();
     void initTelephony();
 
     BatteryState    batteryState;
@@ -89,6 +91,7 @@ private:
 
     const BatteryAgent    *batteryAgent;
     const CellularAgent   *cellularAgent;
+    const FingerAgent     *fingerAgent;
     const TelephonyAgent  *telephonyAgent;
 
     bool     themeIsDark;
@@ -122,6 +125,9 @@ private slots:
     void on_cell_standardBox_currentIndexChanged(int index);
     void on_cell_voiceStatusBox_currentIndexChanged(int index);
     void on_cell_dataStatusBox_currentIndexChanged(int index);
+
+    // Fingerprint
+    void on_finger_touchCkBox_toggled(bool checked);
 
     // Telephony
     void on_tel_startCallButton_clicked();

vicamo/libandroid-emugl

android/skin/qt/ext-finger.cpp
@@ -1,0 +1,42 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/finger-agent.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+#include <QtWidgets>
+
+void ExtendedWindow::initFinger()
+{ }
+
+void ExtendedWindow::on_finger_touchCkBox_toggled(bool checked)
+{
+    bool OK = false;
+    int  id = mExtendedUi->finger_IdBox->toPlainText().toInt(&OK);
+
+    if (checked && !OK) {
+        QErrorMessage *eM = new QErrorMessage;
+        eM->showMessage(QString("The \"Fingerprint ID\" number is invalid"));
+        mExtendedUi->finger_IdBox->setEnabled(true);
+        mExtendedUi->finger_touchCkBox->setChecked(false);
+        return;
+    }
+
+    if (mFingerAgent && mFingerAgent->setTouch) {
+        mFingerAgent->setTouch(checked, id);
+    }
+
+    // Don't allow changing the ID while a touch is active
+    mExtendedUi->finger_IdBox->setEnabled( !checked );
+}

android/skin/qt/extended-window.cpp
@@ -26,6 +26,7 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     mToolWindow(tW),
     mBatteryAgent  (agentPtr ? agentPtr->battery   : NULL),
     mCellularAgent (agentPtr ? agentPtr->cellular  : NULL),
+    mFingerAgent   (agentPtr ? agentPtr->finger    : NULL),
     mTelephonyAgent(agentPtr ? agentPtr->telephony : NULL),
     mExtendedUi(new Ui::ExtendedControls)
 {
@@ -39,6 +40,7 @@ ExtendedWindow::ExtendedWindow(EmulatorQtWindow *eW, ToolWindow *tW, const UiEmu
     // Do any sub-window-specific initialization
     initBattery();
     initCellular();
+    initFinger();
     initTelephony();
 
     move(mParentWindow->geometry().right() + 40,

android/skin/qt/extended-window.h
@@ -25,6 +25,7 @@
 
 #include "android/battery-agent.h"
 #include "android/cellular-agent.h"
+#include "android/finger-agent.h"
 #include "android/telephony-agent.h"
 
 class EmulatorQtWindow;
@@ -82,6 +83,7 @@ private:
 
     void initBattery();
     void initCellular();
+    void initFinger();
     void initTelephony();
 
     BatteryState    mBatteryState;
@@ -89,6 +91,7 @@ private:
 
     const BatteryAgent    *mBatteryAgent;
     const CellularAgent   *mCellularAgent;
+    const FingerAgent     *mFingerAgent;
     const TelephonyAgent  *mTelephonyAgent;
 
     Ui::ExtendedControls *mExtendedUi;
@@ -120,6 +123,9 @@ private slots:
     void on_cell_standardBox_currentIndexChanged(int index);
     void on_cell_voiceStatusBox_currentIndexChanged(int index);
     void on_cell_dataStatusBox_currentIndexChanged(int index);
+
+    // Fingerprint
+    void on_finger_touchCkBox_toggled(bool checked);
 
     // Telephony
     void on_tel_startCallButton_clicked();
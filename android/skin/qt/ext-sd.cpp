aosp-mirror/platform_external_qemu

Add preliminary "SD Card" functionality
This implementation is rudimentary.
We may remove this, rather than complete the implementation.

Change-Id: If1569f8eaeb3daf9281d3bb921712570565d0fbc

android/skin/qt/ext-sd.cpp
@@ -1,0 +1,40 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/avd/info.h"
+#include "android/globals.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+#include <QtWidgets>
+
+void ExtendedWindow::initSd()
+{
+    char* cardPath = NULL;
+
+    // Do we have an SD card?
+    if (android_avdInfo) {
+        cardPath = avdInfo_getSdCardPath(android_avdInfo);
+    }
+
+    if (cardPath && cardPath[0] != '\0') {
+
+        // TODO: Need to use the ADB directory service to get the correct QFileSystemModel
+        QFileSystemModel *myModel = new QFileSystemModel;
+        myModel->setRootPath(QDir::currentPath());
+
+        extendedUi->sd_treeView->setModel(myModel);
+
+        extendedUi->sd_treeView->setRootIndex(myModel->index(QDir::currentPath()));
+    }
+}

vicamo/libandroid-emugl

android/skin/qt/ext-sd.cpp
@@ -1,0 +1,40 @@
+/* Copyright (C) 2015 The Android Open Source Project
+ **
+ ** This software is licensed under the terms of the GNU General Public
+ ** License version 2, as published by the Free Software Foundation, and
+ ** may be copied, distributed, and modified under those terms.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ */
+
+#include "android/avd/info.h"
+#include "android/globals.h"
+#include "android/skin/qt/emulator-qt-window.h"
+#include "extended-window.h"
+#include "ui_extended.h"
+
+#include <QtWidgets>
+
+void ExtendedWindow::initSd()
+{
+    char* cardPath = NULL;
+
+    // Do we have an SD card?
+    if (android_avdInfo) {
+        cardPath = avdInfo_getSdCardPath(android_avdInfo);
+    }
+
+    if (cardPath && cardPath[0] != '\0') {
+
+        // TODO: Need to use the ADB directory service to get the correct QFileSystemModel
+        QFileSystemModel *myModel = new QFileSystemModel;
+        myModel->setRootPath(QDir::currentPath());
+
+        mExtendedUi->sd_treeView->setModel(myModel);
+
+        mExtendedUi->sd_treeView->setRootIndex(myModel->index(QDir::currentPath()));
+    }
+}
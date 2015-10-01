aosp-mirror/platform_external_qemu

Add 560dpi constant to emulator
Change-Id: I827d8368cc528f0dc0d7e01e9c5fd77ab189f172

android/hw-lcd.c
@@ -37,8 +37,10 @@ hwLcd_setBootProperty(int density)
             density = LCD_DENSITY_360DPI;
         else if (density < (LCD_DENSITY_400DPI + LCD_DENSITY_XXHDPI)/2)
             density = LCD_DENSITY_400DPI;
-        else if (density < (LCD_DENSITY_XXHDPI + LCD_DENSITY_XXXHDPI)/2)
+        else if (density < (LCD_DENSITY_XXHDPI + LCD_DENSITY_560HDPI)/2)
             density = LCD_DENSITY_XXHDPI;
+        else if (density < (LCD_DENSITY_560HDPI + LCD_DENSITY_XXXHDPI)/2)
+            density = LCD_DENSITY_560HDPI;
         else
             density = LCD_DENSITY_XXXHDPI;
     }

vicamo/libandroid-emugl

android/hw-lcd.c
@@ -37,8 +37,10 @@ hwLcd_setBootProperty(int density)
             density = LCD_DENSITY_360DPI;
         else if (density < (LCD_DENSITY_400DPI + LCD_DENSITY_XXHDPI)/2)
             density = LCD_DENSITY_400DPI;
-        else if (density < (LCD_DENSITY_XXHDPI + LCD_DENSITY_XXXHDPI)/2)
+        else if (density < (LCD_DENSITY_XXHDPI + LCD_DENSITY_560DPI)/2)
             density = LCD_DENSITY_XXHDPI;
+        else if (density < (LCD_DENSITY_560DPI + LCD_DENSITY_XXXHDPI)/2)
+            density = LCD_DENSITY_560DPI;
         else
             density = LCD_DENSITY_XXXHDPI;
     }
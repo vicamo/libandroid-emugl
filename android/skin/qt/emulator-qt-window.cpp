aosp-mirror/platform_external_qemu

[Qt UI] Hanging after window move fix on Windows
The bug turns out to be a result of strange interation of
Qt internals and Windows message and input loops. For GPU
emulation we were creating a native Win32 window, make it
a child of main Qt window and draw on that child rendering
window.
The trick is that this rendering window is created in a
separate thread, so in Windows it is owned by that thread
and messages have to be processed there. Together with Qt's
issue with requiring all UI interactions to come from main
UI thread, it just blows off - on a window move Qt hangs
deep in Windows dlls processing messages for both itself and
child native window - which is not supposet to happen.

The fix is quite simple:
1. Create an API to run a generic function in a way the current
 UI subsystem is OK with. For Qt it runs the function on its
 main thread; SDL just executed it on the thread it was called

2. Create the rendering window using function from #1 - it
 makes it to be a part of main UI thread on Qt, so everything
 just works

Change-Id: Ib4684d1d755a0da377e93582fe5b8f42ba1b7088

android/skin/qt/emulator-qt-window.cpp
@@ -77,6 +77,7 @@ EmulatorQtWindow::EmulatorQtWindow(QWidget *parent) :
     QObject::connect(this, &EmulatorQtWindow::setWindowPos, this, &EmulatorQtWindow::slot_setWindowPos);
     QObject::connect(this, &EmulatorQtWindow::setTitle, this, &EmulatorQtWindow::slot_setWindowTitle);
     QObject::connect(this, &EmulatorQtWindow::showWindow, this, &EmulatorQtWindow::slot_showWindow);
+    QObject::connect(this, &EmulatorQtWindow::runOnUiThread, this, &EmulatorQtWindow::slot_runOnUiThread);
     QObject::connect(QApplication::instance(), &QCoreApplication::aboutToQuit, this, &EmulatorQtWindow::slot_clearInstance);
 
     resize_timer.setSingleShot(true);
@@ -641,4 +642,9 @@ void EmulatorQtWindow::simulateSetScale(double scale)
     SkinEvent *event = createSkinEvent(kEventSetScale);
     event->u.resize.scale = scale;
     slot_queueEvent(event);
+}
+
+void EmulatorQtWindow::slot_runOnUiThread(GenericFunction* f, void* data, QSemaphore* semaphore) {
+    (*f)(data);
+    if (semaphore) semaphore->release();
 }

android/skin/qt/emulator-qt-window.h
@@ -105,6 +105,10 @@ signals:
     void setTitle(const QString *title, QSemaphore *semaphore = NULL);
     void showWindow(SkinSurface* surface, const QRect* rect, int is_fullscreen, QSemaphore *semaphore = NULL);
 
+    // Qt doesn't support function pointers in signals/slots natively, but
+    // pointer to function pointer works fine
+    void runOnUiThread(GenericFunction* f, void* data, QSemaphore* semaphore = NULL);
+
 public:
     void screenshot();
     void simulateKeyPress(int keyCode, int modifiers);
@@ -132,6 +136,7 @@ private slots:
     void slot_setWindowPos(int x, int y, QSemaphore *semaphore = NULL);
     void slot_setWindowTitle(const QString *title, QSemaphore *semaphore = NULL);
     void slot_showWindow(SkinSurface* surface, const QRect* rect, int is_fullscreen, QSemaphore *semaphore = NULL);
+    void slot_runOnUiThread(GenericFunction* f, void* data, QSemaphore* semaphore = NULL);
 
     /*
      Here are conventional slots that perform interesting high-level functions in the emulator. These can be hooked up to signals

android/skin/qt/winsys-qt.cpp
@@ -267,6 +267,18 @@ extern void skin_winsys_start(bool, bool)
     new EmulatorQtWindow(NULL);
 }
 
+extern void skin_winsys_run_ui_update(GenericFunction f, void* data) {
+    D(__FUNCTION__);
+    QSemaphore semaphore;
+    EmulatorQtWindow* const window = EmulatorQtWindow::getInstance();
+    if (window == NULL) {
+        D("%s: Could not get window handle", __FUNCTION__);
+        return;
+    }
+    window->runOnUiThread(&f, data, &semaphore);
+    semaphore.acquire();
+}
+
 #ifdef _WIN32
 extern "C" int qt_main(int, char**);
 

android/skin/winsys-sdl2.c
@@ -293,3 +293,8 @@ void skin_winsys_start(bool no_window, bool raw_keys) {
 void skin_winsys_quit(void) {
     SDL_Quit();
 }
+
+void skin_winsys_run_ui_update(GenericFunction f, void* data) {
+    // for SDL it doesn't really matter which thread updates the UI
+    f(data);
+}

android/skin/winsys.h
@@ -73,6 +73,12 @@ void skin_winsys_spawn_thread(StartFunction f, int argc, char** argv);
 // Enter the main event handling loop for the UI subsystem.
 void skin_winsys_enter_main_loop(int argc, char** argv);
 
+// Run some UI update in a way that UI subsystem needs
+// E.g. for Qt UI, it makes sure the function runs on the main Qt UI thread,
+//   as Qt requires any UI interaction to come from there.
+typedef void (*GenericFunction)(void* data);
+void skin_winsys_run_ui_update(GenericFunction f, void* data);
+
 #ifdef __cplusplus
 }
 #endif

vicamo/libandroid-emugl

android/skin/qt/emulator-qt-window.cpp
@@ -77,6 +77,7 @@ EmulatorQtWindow::EmulatorQtWindow(QWidget *parent) :
     QObject::connect(this, &EmulatorQtWindow::setWindowPos, this, &EmulatorQtWindow::slot_setWindowPos);
     QObject::connect(this, &EmulatorQtWindow::setTitle, this, &EmulatorQtWindow::slot_setWindowTitle);
     QObject::connect(this, &EmulatorQtWindow::showWindow, this, &EmulatorQtWindow::slot_showWindow);
+    QObject::connect(this, &EmulatorQtWindow::runOnUiThread, this, &EmulatorQtWindow::slot_runOnUiThread);
     QObject::connect(QApplication::instance(), &QCoreApplication::aboutToQuit, this, &EmulatorQtWindow::slot_clearInstance);
 
     resize_timer.setSingleShot(true);
@@ -641,4 +642,9 @@ void EmulatorQtWindow::simulateSetScale(double scale)
     SkinEvent *event = createSkinEvent(kEventSetScale);
     event->u.resize.scale = scale;
     slot_queueEvent(event);
+}
+
+void EmulatorQtWindow::slot_runOnUiThread(SkinGenericFunction* f, void* data, QSemaphore* semaphore) {
+    (*f)(data);
+    if (semaphore) semaphore->release();
 }

android/skin/qt/emulator-qt-window.h
@@ -105,6 +105,10 @@ signals:
     void setTitle(const QString *title, QSemaphore *semaphore = NULL);
     void showWindow(SkinSurface* surface, const QRect* rect, int is_fullscreen, QSemaphore *semaphore = NULL);
 
+    // Qt doesn't support function pointers in signals/slots natively, but
+    // pointer to function pointer works fine
+    void runOnUiThread(SkinGenericFunction* f, void* data, QSemaphore* semaphore = NULL);
+
 public:
     void screenshot();
     void simulateKeyPress(int keyCode, int modifiers);
@@ -132,6 +136,7 @@ private slots:
     void slot_setWindowPos(int x, int y, QSemaphore *semaphore = NULL);
     void slot_setWindowTitle(const QString *title, QSemaphore *semaphore = NULL);
     void slot_showWindow(SkinSurface* surface, const QRect* rect, int is_fullscreen, QSemaphore *semaphore = NULL);
+    void slot_runOnUiThread(SkinGenericFunction* f, void* data, QSemaphore* semaphore = NULL);
 
     /*
      Here are conventional slots that perform interesting high-level functions in the emulator. These can be hooked up to signals

android/skin/qt/winsys-qt.cpp
@@ -267,6 +267,18 @@ extern void skin_winsys_start(bool, bool)
     new EmulatorQtWindow(NULL);
 }
 
+extern void skin_winsys_run_ui_update(SkinGenericFunction f, void* data) {
+    D(__FUNCTION__);
+    QSemaphore semaphore;
+    EmulatorQtWindow* const window = EmulatorQtWindow::getInstance();
+    if (window == NULL) {
+        D("%s: Could not get window handle", __FUNCTION__);
+        return;
+    }
+    window->runOnUiThread(&f, data, &semaphore);
+    semaphore.acquire();
+}
+
 #ifdef _WIN32
 extern "C" int qt_main(int, char**);
 

android/skin/winsys-sdl2.c
@@ -293,3 +293,8 @@ void skin_winsys_start(bool no_window, bool raw_keys) {
 void skin_winsys_quit(void) {
     SDL_Quit();
 }
+
+void skin_winsys_run_ui_update(SkinGenericFunction f, void* data) {
+    // for SDL it doesn't really matter which thread updates the UI
+    f(data);
+}

android/skin/winsys.h
@@ -73,6 +73,12 @@ void skin_winsys_spawn_thread(StartFunction f, int argc, char** argv);
 // Enter the main event handling loop for the UI subsystem.
 void skin_winsys_enter_main_loop(int argc, char** argv);
 
+// Run some UI update in a way that UI subsystem needs
+// E.g. for Qt UI, it makes sure the function runs on the main Qt UI thread,
+//   as Qt requires any UI interaction to come from there.
+typedef void (*SkinGenericFunction)(void* data);
+void skin_winsys_run_ui_update(SkinGenericFunction f, void* data);
+
 #ifdef __cplusplus
 }
 #endif
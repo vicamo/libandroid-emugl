aosp-mirror/platform_external_qemu

Adding breakpad and symdump to emulator build flow
Adding breakpad build & link variables to makefiles
Use dump_syms tool on all released executables and shared libraries
Fixed dump_syms tool through breakpad patch on invalid input file
Updating package_release to copy symbols directory

Change-Id: I957e75a72e94401fc703f7bda41f7e1b2bb69fea

android-configure.sh
@@ -102,6 +102,7 @@ esac
 
 HOST_ARCH=$BUILD_ARCH
 HOST_TAG=$HOST_OS-$HOST_ARCH
+LOCAL_HOST_TAG=$HOST_TAG
 
 #### Toolchain support
 ####
@@ -589,6 +590,22 @@ mkdir -p $OUT_DIR/lib
 cp -f "$CACERTS_FILE" "$OUT_DIR/lib/"
 
 ###
+###  Breakpad probe
+###
+BREAKPAD_PREBUILTS_DIR=$AOSP_PREBUILTS_DIR/android-emulator-build/common/breakpad
+if [ -d "$BREAKPAD_PREBUILTS_DIR" ]; then
+    log "BREAKPAD prebuilts dir: $BREAKPAD_PREBUILTS_DIR"
+    if [ "$OPTION_MINGW" = "yes" ] ; then
+      DUMPSYMS=$BREAKPAD_PREBUILTS_DIR/$LOCAL_HOST_TAG/bin/dump_syms_dwarf
+    else
+      ##Mac and Linux builds
+      DUMPSYMS=$BREAKPAD_PREBUILTS_DIR/$LOCAL_HOST_TAG/bin/dump_syms
+    fi
+else
+    panic "Missing prebuilts directory (please run build-breakpad.sh): $BREAKPAD_PREBUILTS_DIR"
+fi
+
+###
 ###  Qt probe
 ###
 QT_PREBUILTS_DIR=
@@ -711,6 +728,7 @@ echo "HOST_CXX    := $CXX" >> $config_mk
 echo "HOST_LD     := $LD" >> $config_mk
 echo "HOST_AR     := $AR" >> $config_mk
 echo "HOST_WINDRES:= $WINDRES" >> $config_mk
+echo "HOST_DUMPSYMS:= $DUMPSYMS" >> $config_mk
 echo "OBJS_DIR    := $OUT_DIR" >> $config_mk
 echo "" >> $config_mk
 echo "HOST_PREBUILT_TAG := $HOST_TAG" >> $config_mk
@@ -728,6 +746,7 @@ echo "BUILD_CXX         := $BUILD_CXX" >> $config_mk
 echo "BUILD_LD          := $BUILD_LD" >> $config_mk
 echo "BUILD_CFLAGS      := $BUILD_CFLAGS" >> $config_mk
 echo "BUILD_LDFLAGS     := $BUILD_LDFLAGS" >> $config_mk
+echo "BUILD_DUMPSYMS    := $DUMPSYMS" >> $config_mk
 
 PWD=`pwd`
 echo "SRC_PATH          := $PWD" >> $config_mk
@@ -748,6 +767,7 @@ fi
 
 echo "LIBXML2_PREBUILTS_DIR := $LIBXML2_PREBUILTS_DIR" >> $config_mk
 echo "LIBCURL_PREBUILTS_DIR := $LIBCURL_PREBUILTS_DIR" >> $config_mk
+echo "BREAKPAD_PREBUILTS_DIR := $BREAKPAD_PREBUILTS_DIR" >> $config_mk
 
 if [ $OPTION_DEBUG = yes ] ; then
     echo "BUILD_DEBUG_EMULATOR := true" >> $config_mk

android/build/symbols.make
@@ -1,0 +1,11 @@
+LOCAL_BUILT_MODULE_SYM := $(LOCAL_BUILT_MODULE).sym
+$(LOCAL_BUILT_MODULE_SYM): PRIVATE_SYMTOOL := $(LOCAL_SYMTOOL)
+$(LOCAL_BUILT_MODULE_SYM): $(LOCAL_BUILT_MODULE)
+	@ echo "Symbol file: $@"
+	@ $(PRIVATE_SYMTOOL) $< > $@ && \
+	SYMB_CODE=`head -n1 $@ | cut -d" " -f4` && \
+	SYMB_NAME=`head -n1 $@ | cut -d" " -f5` && \
+	mkdir -p $(SYMBOLS_DIR)/$$SYMB_CODE/$$SYMB_NAME && \
+	cp $@ $(SYMBOLS_DIR)/$$SYMB_CODE/$$SYMB_NAME
+
+$(if $(LOCAL_GENERATE_SYMBOLS), $(eval SYMBOLS+=$(LOCAL_BUILT_MODULE_SYM)))

vicamo/libandroid-emugl

android-configure.sh
@@ -102,6 +102,7 @@ esac
 
 HOST_ARCH=$BUILD_ARCH
 HOST_TAG=$HOST_OS-$HOST_ARCH
+BUILD_TAG=$HOST_TAG
 
 #### Toolchain support
 ####
@@ -589,6 +590,22 @@ mkdir -p $OUT_DIR/lib
 cp -f "$CACERTS_FILE" "$OUT_DIR/lib/"
 
 ###
+###  Breakpad probe
+###
+BREAKPAD_PREBUILTS_DIR=$AOSP_PREBUILTS_DIR/android-emulator-build/common/breakpad
+if [ -d "$BREAKPAD_PREBUILTS_DIR" ]; then
+    log "BREAKPAD prebuilts dir: $BREAKPAD_PREBUILTS_DIR"
+    if [ "$OPTION_MINGW" = "yes" ] ; then
+        DUMPSYMS=$BREAKPAD_PREBUILTS_DIR/$BUILD_TAG/bin/dump_syms_dwarf
+    else
+        ##Mac and Linux builds
+        DUMPSYMS=$BREAKPAD_PREBUILTS_DIR/$BUILD_TAG/bin/dump_syms
+    fi
+else
+    panic "Missing prebuilts directory (please run build-breakpad.sh): $BREAKPAD_PREBUILTS_DIR"
+fi
+
+###
 ###  Qt probe
 ###
 QT_PREBUILTS_DIR=
@@ -711,6 +728,7 @@ echo "HOST_CXX    := $CXX" >> $config_mk
 echo "HOST_LD     := $LD" >> $config_mk
 echo "HOST_AR     := $AR" >> $config_mk
 echo "HOST_WINDRES:= $WINDRES" >> $config_mk
+echo "HOST_DUMPSYMS:= $DUMPSYMS" >> $config_mk
 echo "OBJS_DIR    := $OUT_DIR" >> $config_mk
 echo "" >> $config_mk
 echo "HOST_PREBUILT_TAG := $HOST_TAG" >> $config_mk
@@ -728,6 +746,7 @@ echo "BUILD_CXX         := $BUILD_CXX" >> $config_mk
 echo "BUILD_LD          := $BUILD_LD" >> $config_mk
 echo "BUILD_CFLAGS      := $BUILD_CFLAGS" >> $config_mk
 echo "BUILD_LDFLAGS     := $BUILD_LDFLAGS" >> $config_mk
+echo "BUILD_DUMPSYMS    := $DUMPSYMS" >> $config_mk
 
 PWD=`pwd`
 echo "SRC_PATH          := $PWD" >> $config_mk
@@ -748,6 +767,7 @@ fi
 
 echo "LIBXML2_PREBUILTS_DIR := $LIBXML2_PREBUILTS_DIR" >> $config_mk
 echo "LIBCURL_PREBUILTS_DIR := $LIBCURL_PREBUILTS_DIR" >> $config_mk
+echo "BREAKPAD_PREBUILTS_DIR := $BREAKPAD_PREBUILTS_DIR" >> $config_mk
 
 if [ $OPTION_DEBUG = yes ] ; then
     echo "BUILD_DEBUG_EMULATOR := true" >> $config_mk

android/build/symbols.make
@@ -1,0 +1,11 @@
+LOCAL_BUILT_MODULE_SYM := $(LOCAL_BUILT_MODULE).sym
+$(LOCAL_BUILT_MODULE_SYM): PRIVATE_SYMTOOL := $(LOCAL_SYMTOOL)
+$(LOCAL_BUILT_MODULE_SYM): $(LOCAL_BUILT_MODULE)
+	@ echo "Symbol file: $@"
+	@ $(PRIVATE_SYMTOOL) $< > $@ && \
+	SYMB_CODE=`head -n1 $@ | cut -d" " -f4` && \
+	SYMB_NAME=`head -n1 $@ | cut -d" " -f5` && \
+	mkdir -p $(SYMBOLS_DIR)/$$SYMB_NAME/$$SYMB_CODE && \
+	cp $@ $(SYMBOLS_DIR)/$$SYMB_NAME/$$SYMB_CODE
+
+$(if $(LOCAL_GENERATE_SYMBOLS), $(eval SYMBOLS+=$(LOCAL_BUILT_MODULE_SYM)))
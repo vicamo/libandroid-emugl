aosp-mirror/platform_external_qemu

ui: Eliminate crash when entering trackball mode
b/199190

o Fix the null pointer that caused the crash
  when trying to display the trackball
o Enable trackball mode only if the device has a
  hardware trackball
o Enlarge the display of the trackball
o When the trackball is enabled, receive mouse-movement
  events even with no button click
o Set the values indicating the mouse's motion relative
  to the previous point
o Elminate a null-pointer dereference crash
o Don't describe how to exit trackball mode if we don't
  actually enter trackball mode

Change-Id: If3350bbabe2e492ac5f2a51899f0b601229ecede

android/skin/qt/emulator-qt-window.cpp
@@ -35,6 +35,7 @@
 #include "android/base/memory/ScopedPtr.h"
 #include "android/cpu_accelerator.h"
 #include "android/emulation/control/user_event_agent.h"
+#include "android/emulator-window.h"
 #include "android/globals.h"
 #include "android/skin/event.h"
 #include "android/skin/keycode.h"
@@ -76,6 +77,7 @@ EmulatorQtWindow::EmulatorQtWindow(QWidget *parent) :
         mNextIsZoom(false),
         mGrabKeyboardInput(false),
         mMouseInside(false),
+        mPrevMousePosition(0, 0),
         mMainLoopThread(nullptr),
         mAvdWarningBox(QMessageBox::Information,
                        tr("Recommended AVD"),
@@ -314,8 +316,15 @@ void EmulatorQtWindow::keyReleaseEvent(QKeyEvent *event)
         skin_event->u.text.text[sizeof(skin_event->u.text.text)-1] = 0;
         queueEvent(skin_event);
     }
-}
 
+    // If we enabled trackball mode, tell Qt to always forward mouse movement
+    // events. Otherwise, Qt will forward them only when a button is pressed.
+    EmulatorWindow* ew = emulator_window_get();
+    bool trackballActive = skin_ui_trackballActive(ew->ui);
+    if ( trackballActive != hasMouseTracking() ) {
+        setMouseTracking(trackballActive);
+    }
+}
 void EmulatorQtWindow::mouseMoveEvent(QMouseEvent *event)
 {
     handleMouseEvent(kEventMouseMotion,
@@ -908,8 +917,10 @@ void EmulatorQtWindow::handleMouseEvent(SkinEventType type, SkinMouseButtonType 
     skin_event->u.mouse.button = button;
     skin_event->u.mouse.x = pos.x();
     skin_event->u.mouse.y = pos.y();
-    skin_event->u.mouse.xrel = 0;
-    skin_event->u.mouse.yrel = 0;
+
+    skin_event->u.mouse.xrel = pos.x() - mPrevMousePosition.x();
+    skin_event->u.mouse.yrel = pos.y() - mPrevMousePosition.y();
+    mPrevMousePosition = pos;
 
     queueEvent(skin_event);
 }

android/skin/ui.c
@@ -299,9 +299,11 @@ _skin_ui_handle_key_command(void* opaque, SkinKeyCommand command, int  down)
         break;
 
     case SKIN_KEY_COMMAND_TOGGLE_TRACKBALL:
-        ui->show_trackball = !ui->show_trackball;
-        skin_window_show_trackball(ui->window, ui->show_trackball);
-        skin_ui_reset_title(ui);
+        if (ui->ui_params.enable_trackball) {
+            ui->show_trackball = !ui->show_trackball;
+            skin_window_show_trackball(ui->window, ui->show_trackball);
+            skin_ui_reset_title(ui);
+        }
         break;
 
     case SKIN_KEY_COMMAND_ONION_ALPHA_UP:
@@ -512,4 +514,8 @@ void skin_ui_set_name(SkinUI* ui, const char* name) {
              "%s",
              name);
     skin_ui_reset_title(ui);
+}
+
+bool skin_ui_trackballActive(SkinUI* ui) {
+    return (ui->ui_params.enable_trackball && ui->show_trackball);
 }

android/skin/ui.h
@@ -18,6 +18,8 @@
 
 #include <stdbool.h>
 
+ANDROID_BEGIN_HEADER
+
 typedef struct SkinUI SkinUI;
 
 // Avoid including headers if possible.
@@ -127,3 +129,9 @@ struct SkinKeyset* skin_ui_get_current_keyset(SkinUI* ui);
 bool skin_ui_process_events(SkinUI* ui);
 
 void skin_ui_reset_title(SkinUI* ui);
+
+// Return true if a trackball exists and the mouse is
+// currently emulating it
+bool skin_ui_trackballActive(SkinUI* ui);
+
+ANDROID_END_HEADER

vicamo/libandroid-emugl

android/skin/qt/emulator-qt-window.cpp
@@ -35,6 +35,7 @@
 #include "android/base/memory/ScopedPtr.h"
 #include "android/cpu_accelerator.h"
 #include "android/emulation/control/user_event_agent.h"
+#include "android/emulator-window.h"
 #include "android/globals.h"
 #include "android/skin/event.h"
 #include "android/skin/keycode.h"
@@ -76,6 +77,7 @@ EmulatorQtWindow::EmulatorQtWindow(QWidget *parent) :
         mNextIsZoom(false),
         mGrabKeyboardInput(false),
         mMouseInside(false),
+        mPrevMousePosition(0, 0),
         mMainLoopThread(nullptr),
         mAvdWarningBox(QMessageBox::Information,
                        tr("Recommended AVD"),
@@ -314,8 +316,15 @@ void EmulatorQtWindow::keyReleaseEvent(QKeyEvent *event)
         skin_event->u.text.text[sizeof(skin_event->u.text.text)-1] = 0;
         queueEvent(skin_event);
     }
-}
 
+    // If we enabled trackball mode, tell Qt to always forward mouse movement
+    // events. Otherwise, Qt will forward them only when a button is pressed.
+    EmulatorWindow* ew = emulator_window_get();
+    bool trackballActive = skin_ui_is_trackball_active(ew->ui);
+    if ( trackballActive != hasMouseTracking() ) {
+        setMouseTracking(trackballActive);
+    }
+}
 void EmulatorQtWindow::mouseMoveEvent(QMouseEvent *event)
 {
     handleMouseEvent(kEventMouseMotion,
@@ -908,8 +917,10 @@ void EmulatorQtWindow::handleMouseEvent(SkinEventType type, SkinMouseButtonType 
     skin_event->u.mouse.button = button;
     skin_event->u.mouse.x = pos.x();
     skin_event->u.mouse.y = pos.y();
-    skin_event->u.mouse.xrel = 0;
-    skin_event->u.mouse.yrel = 0;
+
+    skin_event->u.mouse.xrel = pos.x() - mPrevMousePosition.x();
+    skin_event->u.mouse.yrel = pos.y() - mPrevMousePosition.y();
+    mPrevMousePosition = pos;
 
     queueEvent(skin_event);
 }

android/skin/ui.c
@@ -299,9 +299,11 @@ _skin_ui_handle_key_command(void* opaque, SkinKeyCommand command, int  down)
         break;
 
     case SKIN_KEY_COMMAND_TOGGLE_TRACKBALL:
-        ui->show_trackball = !ui->show_trackball;
-        skin_window_show_trackball(ui->window, ui->show_trackball);
-        skin_ui_reset_title(ui);
+        if (ui->ui_params.enable_trackball) {
+            ui->show_trackball = !ui->show_trackball;
+            skin_window_show_trackball(ui->window, ui->show_trackball);
+            skin_ui_reset_title(ui);
+        }
         break;
 
     case SKIN_KEY_COMMAND_ONION_ALPHA_UP:
@@ -512,4 +514,8 @@ void skin_ui_set_name(SkinUI* ui, const char* name) {
              "%s",
              name);
     skin_ui_reset_title(ui);
+}
+
+bool skin_ui_is_trackball_active(SkinUI* ui) {
+    return (ui->ui_params.enable_trackball && ui->show_trackball);
 }

android/skin/ui.h
@@ -18,6 +18,8 @@
 
 #include <stdbool.h>
 
+ANDROID_BEGIN_HEADER
+
 typedef struct SkinUI SkinUI;
 
 // Avoid including headers if possible.
@@ -127,3 +129,9 @@ struct SkinKeyset* skin_ui_get_current_keyset(SkinUI* ui);
 bool skin_ui_process_events(SkinUI* ui);
 
 void skin_ui_reset_title(SkinUI* ui);
+
+// Return true if a trackball exists and the mouse is
+// currently emulating it
+bool skin_ui_is_trackball_active(SkinUI* ui);
+
+ANDROID_END_HEADER
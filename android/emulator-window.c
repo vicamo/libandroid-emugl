aosp-mirror/platform_external_qemu

Add warning messages for deprecated window size flags.
The "-scale" and "-dpi-device" flags were both only responsible for
setting the size of the emulator window from the command line. The
help messages now reflect that these flags do nothing, and a warning
is printed to inform the user that the flags no longer work.

Change-Id: I530adf1710f34fc81482f31c1c646405ef3a9fb8

android/emulator-window.c
@@ -193,6 +193,14 @@ emulator_window_setup( EmulatorWindow*  emulator )
         return;
     }
 
+    if (emulator->opts->scale) {
+        dwarning("The -scale flag is deprecated and will be ignored.");
+    }
+
+    if (emulator->opts->dpi_device) {
+        dwarning("The -dpi-device flag is deprecated and will be ignored.");
+    }
+
     SkinUIParams my_ui_params = {
         .enable_touch = !androidHwConfig_isScreenNoTouch(android_hw),
         .enable_dpad = android_hw->hw_dPad != 0,
@@ -407,26 +415,6 @@ emulator_window_get_first_framebuffer(EmulatorWindow* emulator)
         }
     SKIN_FILE_LOOP_END_PARTS
     return NULL;
-}
-
-/*
- * Helper routines
- */
-
-static int
-get_device_dpi( AndroidOptions*  opts )
-{
-    int dpi_device = android_hw->hw_lcd_density;
-
-    if (opts->dpi_device != NULL) {
-        char*  end;
-        dpi_device = strtol( opts->dpi_device, &end, 0 );
-        if (end == NULL || *end != 0 || dpi_device <= 0) {
-            fprintf(stderr, "argument for -dpi-device must be a positive integer. Aborting\n" );
-            exit(1);
-        }
-    }
-    return  dpi_device;
 }
 
 /* called periodically to poll for user input events */

android/help.c
@@ -969,15 +969,7 @@ static void
 help_dpi_device(stralloc_t*  out)
 {
     PRINTF(
-    "  use '-dpi-device <dpi>' to specify the screen resolution of the emulated\n"
-    "  device. <dpi> must be an integer between 72 and 1000. the default is taken\n"
-    "  from the skin, if available, or uses the contant value %d (an average of\n"
-    "  several prototypes used during Android development).\n\n", DEFAULT_DEVICE_DPI );
-
-    PRINTF(
-    "  the device resolution can also used to rescale the emulator window with\n"
-    "  the '-scale' option (see -help-scale)\n\n"
-    );
+    "  the '-dpi-device <dpi>' option is deprecated as of Emulator 2.0 and will be ignored\n\n" );
 }
 
 static void
@@ -995,30 +987,7 @@ static void
 help_scale(stralloc_t*  out)
 {
     PRINTF(
-    "  the '-scale <scale>' option is used to scale the emulator window to\n"
-    "  something that better fits the physical dimensions of a real device. this\n"
-    "  can be *very* useful to check that your UI isn't too small to be usable\n"
-    "  on a real device.\n\n"
-
-    "  there are three supported formats for <scale>:\n\n"
-
-    "  * if <scale> is a real number (between 0.1 and 3.0) it is used as a\n"
-    "    scaling factor for the emulator's window.\n\n"
-
-    "  * if <scale> is an integer followed by the suffix 'dpi' (e.g. '110dpi'),\n"
-    "    then it is interpreted as the resolution of your monitor screen. this\n"
-    "    will be divided by the emulated device's resolution to get an absolute\n"
-    "    scale. (see -help-dpi-device for details).\n\n"
-
-    "  * finally, if <scale> is the keyword 'auto', the emulator tries to guess\n"
-    "    your monitor's resolution and automatically adjusts its window\n"
-    "    accordingly\n\n"
-
-    "    NOTE: this process is *very* unreliable, depending on your OS, video\n"
-    "          driver issues and other random system parameters\n\n"
-
-    "  the emulator's scale can be changed anytime at runtime through the control\n"
-    "  console. see the help for the 'window scale' command for details\n\n" );
+    "  the '-scale <scale>' option is deprecated as of Emulator 2.0 and will be ignored\n\n" );
 }
 
 static void

vicamo/libandroid-emugl

android/emulator-window.c
@@ -193,6 +193,14 @@ emulator_window_setup( EmulatorWindow*  emulator )
         return;
     }
 
+    if (emulator->opts->scale) {
+        dwarning("The -scale flag is obsolete and will be ignored.");
+    }
+
+    if (emulator->opts->dpi_device) {
+        dwarning("The -dpi-device flag is obsolete and will be ignored.");
+    }
+
     SkinUIParams my_ui_params = {
         .enable_touch = !androidHwConfig_isScreenNoTouch(android_hw),
         .enable_dpad = android_hw->hw_dPad != 0,
@@ -407,26 +415,6 @@ emulator_window_get_first_framebuffer(EmulatorWindow* emulator)
         }
     SKIN_FILE_LOOP_END_PARTS
     return NULL;
-}
-
-/*
- * Helper routines
- */
-
-static int
-get_device_dpi( AndroidOptions*  opts )
-{
-    int dpi_device = android_hw->hw_lcd_density;
-
-    if (opts->dpi_device != NULL) {
-        char*  end;
-        dpi_device = strtol( opts->dpi_device, &end, 0 );
-        if (end == NULL || *end != 0 || dpi_device <= 0) {
-            fprintf(stderr, "argument for -dpi-device must be a positive integer. Aborting\n" );
-            exit(1);
-        }
-    }
-    return  dpi_device;
 }
 
 /* called periodically to poll for user input events */

android/help.c
@@ -969,15 +969,7 @@ static void
 help_dpi_device(stralloc_t*  out)
 {
     PRINTF(
-    "  use '-dpi-device <dpi>' to specify the screen resolution of the emulated\n"
-    "  device. <dpi> must be an integer between 72 and 1000. the default is taken\n"
-    "  from the skin, if available, or uses the contant value %d (an average of\n"
-    "  several prototypes used during Android development).\n\n", DEFAULT_DEVICE_DPI );
-
-    PRINTF(
-    "  the device resolution can also used to rescale the emulator window with\n"
-    "  the '-scale' option (see -help-scale)\n\n"
-    );
+    "  the '-dpi-device <dpi>' option is obsolete as of Emulator 2.0 and will be ignored\n\n" );
 }
 
 static void
@@ -995,30 +987,7 @@ static void
 help_scale(stralloc_t*  out)
 {
     PRINTF(
-    "  the '-scale <scale>' option is used to scale the emulator window to\n"
-    "  something that better fits the physical dimensions of a real device. this\n"
-    "  can be *very* useful to check that your UI isn't too small to be usable\n"
-    "  on a real device.\n\n"
-
-    "  there are three supported formats for <scale>:\n\n"
-
-    "  * if <scale> is a real number (between 0.1 and 3.0) it is used as a\n"
-    "    scaling factor for the emulator's window.\n\n"
-
-    "  * if <scale> is an integer followed by the suffix 'dpi' (e.g. '110dpi'),\n"
-    "    then it is interpreted as the resolution of your monitor screen. this\n"
-    "    will be divided by the emulated device's resolution to get an absolute\n"
-    "    scale. (see -help-dpi-device for details).\n\n"
-
-    "  * finally, if <scale> is the keyword 'auto', the emulator tries to guess\n"
-    "    your monitor's resolution and automatically adjusts its window\n"
-    "    accordingly\n\n"
-
-    "    NOTE: this process is *very* unreliable, depending on your OS, video\n"
-    "          driver issues and other random system parameters\n\n"
-
-    "  the emulator's scale can be changed anytime at runtime through the control\n"
-    "  console. see the help for the 'window scale' command for details\n\n" );
+    "  the '-scale <scale>' option is obsolete as of Emulator 2.0 and will be ignored\n\n" );
 }
 
 static void
aosp-mirror/platform_external_qemu

[UI] Add a mechanism for recording UI events.
We want to monitor and record UI events for including them
in crash reports or profiling input latency.

Change-Id: I410d728c89ca93da255f61d741848c77302e93b9

android/crashreport/CrashReporter_common.cpp
@@ -183,6 +183,13 @@ bool CrashReporter::attachFile(StringView sourceFullName,
     return path_copy_file(fullName, sourceFullName.c_str()) >= 0;
 }
 
+bool CrashReporter::onCrash() {
+    if (CrashReporter::get()->mUICrashCallback) {
+        CrashReporter::get()->mUICrashCallback();
+    }
+    return CrashReporter::get()->onCrashPlatformSpecific();
+}
+
 }  // namespace crashreport
 }  // namespace android
 

android/skin/qt/event-subscriber.cpp
@@ -1,0 +1,38 @@
+// Copyright 2016 The Android Open Source Project
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/skin/qt/event-subscriber.h"
+
+EventSubscriber::EventSubscriber(EventCapturer* ecap) : mEventCapturer(ecap) {}
+
+void EventSubscriber::startRecording(QObject* target) {
+    if (mEventCapturer) {
+        mTokens[target] = mEventCapturer->subscribeToEvents(
+                target,
+                [this](const QObject* o){ return this->objectPredicate(o); },
+                eventTypes(),
+                [this](const QObject* target, const QEvent* event) {
+                    this->processEvent(target, event);
+                });
+        connect(target, SIGNAL(destroyed()), this, SLOT(stopRecording()));
+    }
+}
+
+void EventSubscriber::stopRecording(QObject* target) {
+    mTokens.erase(target);
+}
+
+void EventSubscriber::stopRecording() {
+    auto target = sender();
+    if (target) {
+        stopRecording(target);
+    }
+}
+

vicamo/libandroid-emugl

android/crashreport/CrashReporter_common.cpp
@@ -183,6 +183,13 @@ bool CrashReporter::attachFile(StringView sourceFullName,
     return path_copy_file(fullName, sourceFullName.c_str()) >= 0;
 }
 
+bool CrashReporter::onCrash() {
+    if (CrashReporter::get()->mCrashCallback) {
+        CrashReporter::get()->mCrashCallback();
+    }
+    return CrashReporter::get()->onCrashPlatformSpecific();
+}
+
 }  // namespace crashreport
 }  // namespace android
 

android/skin/qt/event-subscriber.cpp
@@ -1,0 +1,37 @@
+// Copyright 2016 The Android Open Source Project
+// This software is licensed under the terms of the GNU General Public
+// License version 2, as published by the Free Software Foundation, and
+// may be copied, distributed, and modified under those terms.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+#include "android/skin/qt/event-subscriber.h"
+
+EventSubscriber::EventSubscriber(EventCapturer* ecap) : mEventCapturer(ecap) {}
+
+void EventSubscriber::startRecording(QObject* target) {
+    if (mEventCapturer) {
+        mTokens[target] = mEventCapturer->subscribeToEvents(
+                target,
+                [this](const QObject* o){ return this->objectPredicate(o); },
+                eventTypes(),
+                [this](const QObject* target, const QEvent* event) {
+                    this->processEvent(target, event);
+                });
+        connect(target, SIGNAL(destroyed()), this, SLOT(stopRecordingForEmitter()));
+    }
+}
+
+void EventSubscriber::stopRecording(QObject* target) {
+    mTokens.erase(target);
+}
+
+void EventSubscriber::stopRecordingForEmitter() {
+    if (auto target = sender()) {
+        stopRecording(target);
+    }
+}
+
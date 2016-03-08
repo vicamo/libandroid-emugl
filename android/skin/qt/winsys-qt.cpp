aosp-mirror/platform_external_qemu

android/skin/: Remove android::base::String usage.
Change-Id: I7be93a7418b230b8e243f97252f09e57fbbc9b08

android/skin/qt/winsys-qt.cpp
@@ -36,6 +36,8 @@
 #include <QThread>
 #include <QWidget>
 
+#include <string>
+
 #ifdef Q_OS_LINUX
 // This include needs to be after all the Qt includes
 // because it defines macros/types that conflict with
@@ -44,7 +46,6 @@
 #endif
 
 using android::base::System;
-using android::base::String;
 
 #define  DEBUG  1
 
@@ -85,7 +86,7 @@ extern void skin_winsys_enter_main_loop(bool no_window, int argc, char** argv) {
     D("Starting QT main loop\n");
 
     // Make Qt look at the libraries within this installation
-    String qtPath = androidQtGetLibraryDir();
+    std::string qtPath = androidQtGetLibraryDir();
     QStringList pathList(qtPath.c_str());
     QCoreApplication::setLibraryPaths(pathList);
     D("Qt lib path: %s\n", qtPath.c_str());

vicamo/libandroid-emugl

android/skin/qt/winsys-qt.cpp
@@ -37,6 +37,8 @@
 #include <QThread>
 #include <QWidget>
 
+#include <string>
+
 #ifdef Q_OS_LINUX
 // This include needs to be after all the Qt includes
 // because it defines macros/types that conflict with
@@ -50,7 +52,6 @@
 #endif
 
 using android::base::System;
-using android::base::String;
 #ifdef _WIN32
 using android::base::Win32UnicodeString;
 #endif
@@ -342,8 +343,8 @@ void skin_winsys_setup_library_paths() {
     // What's even more interesting is that adding the plugin path here is not
     // enough in itself. It also has to be set through the environment variable
     // or extended unicode characters won't work
-    String qtLibPath = androidQtGetLibraryDir();
-    String qtPluginsPath = androidQtGetPluginsDir();
+    std::string qtLibPath = androidQtGetLibraryDir();
+    std::string qtPluginsPath = androidQtGetPluginsDir();
     QStringList pathList;
     pathList.append(QString::fromUtf8(qtLibPath.c_str()));
     pathList.append(QString::fromUtf8(qtPluginsPath.c_str()));
@@ -457,12 +458,12 @@ int qMain(int argc, char** argv) {
 
     // Store converted strings and pointers to those strings, the pointers are
     // what will become the argv for qt_main
-    std::vector<String> arguments(numArgs);
+    std::vector<std::string> arguments(numArgs);
     std::vector<char*> argumentPointers(numArgs);
 
     for (int i = 0; i < numArgs; ++i) {
         arguments[i] = Win32UnicodeString::convertToUtf8(wideArgv[i]);
-        argumentPointers[i] = reinterpret_cast<char*>(arguments[i].data());
+        argumentPointers[i] = reinterpret_cast<char*>(&arguments[i][0]);
     }
 
     return qt_main(numArgs, &argumentPointers[0]);
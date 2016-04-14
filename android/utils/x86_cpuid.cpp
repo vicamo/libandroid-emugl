aosp-mirror/platform_external_qemu

[emulator-check] Add CPU bittness to the cpu-info query
It looks like Java has issues with calling platform-specific
functions for CPU information detection
That's why this CL adds additional bits for the CPU and OS
bittness

Change-Id: I70bc804647be41d282130a3d773d3e7d92f25444

android/utils/x86_cpuid.cpp
@@ -216,3 +216,26 @@ bool android_get_x86_cpuid_nx_support()
     const uint32_t CPUID_80000001_EDX_NX = (1<<20); // NX support
     return (cpuid_80000001_edx & CPUID_80000001_EDX_NX) != 0;
 }
+
+bool android_get_x86_cpuid_is_64bit_capable()
+{
+#ifdef __x86_64__
+    return true;    // this was easy
+#else
+    const uint32_t bittnessSupportFunc = 0x80000001;
+
+    const auto maxFunc = android_get_x86_cpuid_extended_function_max();
+    if (maxFunc < bittnessSupportFunc) {
+        return false; // if it's that old, it is not a 64-bit one
+    }
+
+    uint32_t edx = 0;
+    android_get_x86_cpuid(bittnessSupportFunc, 0,
+                          nullptr, nullptr, nullptr, &edx);
+    // bit 29 is the 64-bit mode support
+    if ((edx & (1 << 29)) != 0) {
+        return true;
+    }
+    return false;
+#endif
+}

vicamo/libandroid-emugl

android/utils/x86_cpuid.cpp
@@ -216,3 +216,26 @@ bool android_get_x86_cpuid_nx_support()
     const uint32_t CPUID_80000001_EDX_NX = (1<<20); // NX support
     return (cpuid_80000001_edx & CPUID_80000001_EDX_NX) != 0;
 }
+
+bool android_get_x86_cpuid_is_64bit_capable()
+{
+#ifdef __x86_64__
+    return true;    // this was easy
+#else
+    const uint32_t kBitnessSupportFunc = 0x80000001;
+
+    const auto maxFunc = android_get_x86_cpuid_extended_function_max();
+    if (maxFunc < kBitnessSupportFunc) {
+        return false; // if it's that old, it is not a 64-bit one
+    }
+
+    uint32_t edx = 0;
+    android_get_x86_cpuid(kBitnessSupportFunc, 0,
+                          nullptr, nullptr, nullptr, &edx);
+    // bit 29 is the 64-bit mode support
+    if ((edx & (1 << 29)) != 0) {
+        return true;
+    }
+    return false;
+#endif
+}
aosp-mirror/platform_external_qemu

[ui] Display the current GPS location
Provide feedback to the user by displaying the most-recently-
sent location on the Location page of the UI.

The displayed values update when the extended window is created,
when the "Send" button is pressed, and during play back of KML/GPX
files.

When the Emulator starts up, it synchronizes its idea of the
current location with the device. First it queries the device.
If the device reports a location, this is used. Otherwise the
last-sent location from the Emulator UI is used.

This location is sent to the device at start-up.

This change also replaces the "decimal/sexigesimal" radio buttons
with a pull-down.

(Note: The code to query the device for its location and
to parse the result are TODO.)

Change-Id: I12d680c694dfacaf6fb2552bd4d44dc8cbbf4316

android/gps.h
@@ -33,3 +33,20 @@ extern void  android_gps_send_nmea( const char*  sentence );
 extern void  android_gps_send_location(double latitude, double longitude,
                                        double metersElevation, int nSatellites,
                                        const struct timeval *time);
+
+////////////////////////////////////////////////////////////
+//
+// android_gps_get_location
+//
+// Get the devices' current GPS location.
+//
+// Outputs:
+//    Return value:     1 on success, 0 if failed
+//
+//    Valid only if the return is 1:
+//    *latitude:        Degrees
+//    *longitude:       Degrees
+//    *metersElevation: Meters above sea level
+//    *nSatellites:     Number of satellites used
+extern int android_gps_get_location(double* latitude, double* longitude,
+                                    double* metersElevation, int* nSatellites);

android/skin/qt/extended-pages/location-page.h
@@ -31,6 +31,15 @@ public:
     bool isLoadingGeoData() const { return mNowLoadingGeoData; }
     void requestStopLoadingGeoData() { mGeoDataLoadingStopRequested = true; }
 
+    static void getDeviceLocation(const QAndroidLocationAgent* locAgent,
+                                  double* pLatitude,
+                                  double* pLongitude,
+                                  double* pAltitude);
+    static void sendLocationToDevice(const QAndroidLocationAgent* locAgent,
+                                     double latitude,
+                                     double longitude,
+                                     double altitude);
+
 signals:
     void geoDataLoadingFinished();
 
@@ -38,10 +47,7 @@ private slots:
     void on_loc_GpxKmlButton_clicked();
     void on_loc_pathTable_cellChanged(int row, int col);
     void on_loc_playStopButton_clicked();
-    void on_loc_decimalModeSwitch_clicked();
-    void on_loc_decimalModeSwitch_toggled(bool checked);
-    void on_loc_sexagesimalModeSwitch_clicked();
-    void on_loc_sexagesimalModeSwitch_toggled(bool checked);
+    void on_loc_modeSwitch_currentIndexChanged(int index);
     void on_loc_sendPointButton_clicked();
     void on_loc_longitudeInput_valueChanged(double);
     void on_loc_latitudeInput_valueChanged(double);
@@ -78,6 +84,8 @@ private:
                              int row,
                              int col,
                              QString* outErrorMessage);
+    void updateDisplayedLocation(double lat, double lon, double alt);
+
 
     std::unique_ptr<Ui::LocationPage> mUi;
     const QAndroidLocationAgent* mLocationAgent;

vicamo/libandroid-emugl

android/gps.h
@@ -33,3 +33,21 @@ extern void  android_gps_send_nmea( const char*  sentence );
 extern void  android_gps_send_location(double latitude, double longitude,
                                        double metersElevation, int nSatellites,
                                        const struct timeval *time);
+
+////////////////////////////////////////////////////////////
+//
+// android_gps_get_location
+//
+// Get the devices' current GPS location.
+//
+// Outputs:
+//    Return value:     1 on success, 0 if failed
+//
+//    Valid only if the return is 1:
+//    *outLatitude:        Degrees
+//    *outLongitude:       Degrees
+//    *outMetersElevation: Meters above sea level
+//    *outNSatellites:     Number of satellites used
+// Null 'out' pointers are ignored.
+extern int android_gps_get_location(double* outLatitude, double* outLongitude,
+                                    double* outMetersElevation, int* outNSatellites);

android/skin/qt/extended-pages/location-page.h
@@ -31,6 +31,15 @@ public:
     bool isLoadingGeoData() const { return mNowLoadingGeoData; }
     void requestStopLoadingGeoData() { mGeoDataLoadingStopRequested = true; }
 
+    static void getDeviceLocation(const QAndroidLocationAgent* locAgent,
+                                  double* pOutLatitude,
+                                  double* pOutLongitude,
+                                  double* pOutAltitude);
+    static void sendLocationToDevice(const QAndroidLocationAgent* locAgent,
+                                     double latitude,
+                                     double longitude,
+                                     double altitude);
+
 signals:
     void geoDataLoadingFinished();
 
@@ -38,10 +47,7 @@ private slots:
     void on_loc_GpxKmlButton_clicked();
     void on_loc_pathTable_cellChanged(int row, int col);
     void on_loc_playStopButton_clicked();
-    void on_loc_decimalModeSwitch_clicked();
-    void on_loc_decimalModeSwitch_toggled(bool checked);
-    void on_loc_sexagesimalModeSwitch_clicked();
-    void on_loc_sexagesimalModeSwitch_toggled(bool checked);
+    void on_loc_modeSwitch_currentIndexChanged(int index);
     void on_loc_sendPointButton_clicked();
     void on_loc_longitudeInput_valueChanged(double);
     void on_loc_latitudeInput_valueChanged(double);
@@ -78,6 +84,8 @@ private:
                              int row,
                              int col,
                              QString* outErrorMessage);
+    void updateDisplayedLocation(double lat, double lon, double alt);
+
 
     std::unique_ptr<Ui::LocationPage> mUi;
     const QAndroidLocationAgent* mLocationAgent;
aosp-mirror/platform_external_qemu

[base] Implement Vista+ condition variable and dynamic selection
This CL adds a code to probe the current Windows and pick either
the old custom code for the condition variable (XP-) or use the
one from WinAPI (Vista+)
Selection is based on the presence of Vista-specific API
functions, so there's no OS version checking involved.

Change-Id: I0f859b70c06bbfd1dc39196afd3abf0e25dcab6e

android/base/synchronization/ConditionVariable_win32.cpp
@@ -27,7 +27,8 @@ namespace base {
 
 namespace {
 
-// Helper class which implements a free list of event handles.
+// Helper class which implements a free list of event handles. Needed in a
+// pre-Vista code.
 class WaitEventStorage {
 public:
     HANDLE alloc() {
@@ -50,7 +51,7 @@ public:
             mFreeHandles.emplace_back(h);
         } else {
             lock.unlock();
-            CloseHandle(h); // don't collect too many open events
+            CloseHandle(h);  // don't collect too many open events
         }
     }
 
@@ -61,43 +62,170 @@ private:
 
 LazyInstance<WaitEventStorage> sWaitEvents = LAZY_INSTANCE_INIT;
 
-}  // namespace
+////////////////////////////////////////////////////////////////////////////////
+// An interface for the OS-specific condition variable implementation.
+class ConditionVariableImpl {
+public:
+    virtual void initCV(ConditionVariable::Data* cvData) const = 0;
+    virtual void waitCV(ConditionVariable::Data* cvData, Lock* lock) const = 0;
+    virtual void signalCV(ConditionVariable::Data* cvData) const = 0;
+    virtual void destroyCV(ConditionVariable::Data* cvData) const = 0;
 
-ConditionVariable::~ConditionVariable() {
-    // Don't lock anything here: if there's someone blocked on |this| while
-    // we're in a dtor in a different thread, the code is already heavily
-    // bugged. Lock here won't save it.
-}
+protected:
+    ~ConditionVariableImpl() noexcept = default;
+};
 
-void ConditionVariable::wait(Lock* userLock) {
-    // Grab new waiter event handle.
-    HANDLE handle = sWaitEvents->alloc();
-    {
-        AutoLock lock(mLock);
-        mWaiters.emplace_back(handle);
+// Fallback code for pre-Vista OS version - use handcrafted code.
+class ConditionVariableImplXp final : public ConditionVariableImpl {
+public:
+    virtual void initCV(ConditionVariable::Data* cvData) const override final {
+        new (&cvData->xp) ConditionVariable::XpCV();
     }
+    virtual void waitCV(ConditionVariable::Data* cvData,
+                        Lock* userLock) const override final {
+        // Grab new waiter event handle.
+        HANDLE handle = sWaitEvents->alloc();
+        {
+            AutoLock lock(cvData->xp.mLock);
+            cvData->xp.mWaiters.emplace_back(handle);
+        }
 
-    // Unlock user lock then wait for event.
-    userLock->unlock();
-    WaitForSingleObject(handle, INFINITE);
-    // NOTE: The handle has been removed from mWaiters here,
-    // see signal() below. Close/recycle the event.
-    sWaitEvents->free(handle);
-    userLock->lock();
-}
-
-void ConditionVariable::signal() {
-    android::base::AutoLock lock(mLock);
-    if (!mWaiters.empty()) {
+        // Unlock user lock then wait for event.
+        userLock->unlock();
+        WaitForSingleObject(handle, INFINITE);
+        // NOTE: The handle has been removed from mWaiters here,
+        // see signal() below. Close/recycle the event.
+        sWaitEvents->free(handle);
+        userLock->lock();
+    }
+    virtual void signalCV(
+            ConditionVariable::Data* cvData) const override final {
+        android::base::AutoLock lock(cvData->xp.mLock);
+        if (cvData->xp.mWaiters.empty()) {
+            return;
+        }
         // NOTE: This wakes up the thread that went to sleep most
         //       recently (LIFO) for performance reason. For better
         //       fairness, using (FIFO) would be appropriate.
-        HANDLE handle = mWaiters.back().release();
-        mWaiters.pop_back();
+        HANDLE handle = cvData->xp.mWaiters.back().release();
+        cvData->xp.mWaiters.pop_back();
         lock.unlock();
         SetEvent(handle);
         // NOTE: The handle will be closed/recycled by the waiter.
     }
+    virtual void destroyCV(
+            ConditionVariable::Data* cvData) const override final {
+        cvData->xp.ConditionVariable::XpCV::~XpCV();
+    }
+};
+
+// A set of Vista+ APIs for condition variables: function types and pointers.
+using InitCV = void(WINAPI*)(ConditionVariable::VistaCV*);
+using SleepCsCV = BOOL(WINAPI*)(ConditionVariable::VistaCV*,
+                                CRITICAL_SECTION*,
+                                DWORD);
+using SleepSrwLockCV = BOOL(WINAPI*)(ConditionVariable::VistaCV*,
+                                     void*,
+                                     DWORD,
+                                     ULONG);
+using WakeCV = void(WINAPI*)(ConditionVariable::VistaCV*);
+
+static InitCV sInitCV = nullptr;
+static SleepCsCV sSleepCsCV = nullptr;
+static SleepSrwLockCV sSleepSrwLockCV = nullptr;
+static WakeCV sWakeAllCV = nullptr;
+static WakeCV sWakeOneCV = nullptr;
+
+// Vista+ code for the condition variable, using the raw Windows API.
+class ConditionVariableImplVista final : public ConditionVariableImpl {
+public:
+    virtual void initCV(ConditionVariable::Data* cvData) const override final {
+        sInitCV(&cvData->vista);
+    }
+    virtual void waitCV(ConditionVariable::Data* cvData,
+                        Lock* userLock) const override final {
+        sSleepCsCV(&cvData->vista, &userLock->mLock, INFINITE);
+    }
+    virtual void signalCV(
+            ConditionVariable::Data* cvData) const override final {
+        sWakeOneCV(&cvData->vista);
+    }
+    virtual void destroyCV(
+            ConditionVariable::Data* cvData) const override final {
+        ;  // there's no special function for condition variable destruction
+    }
+};
+
+// Instances of both implementations and a pointer that will point to the one
+// to use on the current OS.
+const ConditionVariableImplXp sImplXp;
+const ConditionVariableImplVista sImplVista;
+const ConditionVariableImpl* sImpl = nullptr;
+
+// A very simple class with the purpose of initializing the |sImpl| into
+// the proper implementation based on a dynamic function loading result.
+class ApiVersionSelector final {
+public:
+    ApiVersionSelector() {
+        static const wchar_t kDllName[] = L"kernel32.dll";
+
+        HMODULE kernel32 = ::GetModuleHandleW(kDllName);
+        if (!kernel32) {
+            kernel32 = ::LoadLibraryW(kDllName);
+            if (!kernel32) {
+                sImpl = &sImplXp;
+                return;  // well, this is a very strange breed of Windows...
+            }
+        }
+
+        const auto init = (InitCV)::GetProcAddress(
+                kernel32, "InitializeConditionVariable");
+        const auto sleepCs = (SleepCsCV)::GetProcAddress(
+                kernel32, "SleepConditionVariableCS");
+        const auto sleepSrw = (SleepSrwLockCV)::GetProcAddress(
+                kernel32, "SleepConditionVariableSRW");
+        const auto wakeOne =
+                (WakeCV)::GetProcAddress(kernel32, "WakeConditionVariable");
+        const auto wakeAll =
+                (WakeCV)::GetProcAddress(kernel32, "WakeAllConditionVariable");
+        if (!init || !sleepCs || !sleepSrw || !wakeOne || !wakeAll) {
+            sImpl = &sImplXp;
+            return;
+        }
+
+        sInitCV = init;
+        sSleepCsCV = sleepCs;
+        sSleepSrwLockCV = sleepSrw;
+        sWakeOneCV = wakeOne;
+        sWakeAllCV = wakeAll;
+        sImpl = &sImplVista;
+    }
+};
+
+LazyInstance<ApiVersionSelector> sApiLoader = LAZY_INSTANCE_INIT;
+
+}  // namespace
+
+// Actual ConditionVariable members - these just forward all calls to |sImpl|.
+
+ConditionVariable::ConditionVariable() {
+    if (!sImpl) {
+        sApiLoader.get();
+    }
+    assert(sImpl);
+    sImpl->initCV(&mData);
+}
+
+ConditionVariable::~ConditionVariable() {
+    sImpl->destroyCV(&mData);
+}
+
+void ConditionVariable::wait(Lock* userLock) {
+    sImpl->waitCV(&mData, userLock);
+}
+
+void ConditionVariable::signal() {
+    sImpl->signalCV(&mData);
 }
 
 }  // namespace base

vicamo/libandroid-emugl

android/base/synchronization/ConditionVariable_win32.cpp
@@ -27,7 +27,8 @@ namespace base {
 
 namespace {
 
-// Helper class which implements a free list of event handles.
+// Helper class which implements a free list of event handles. Needed in a
+// pre-Vista code.
 class WaitEventStorage {
 public:
     HANDLE alloc() {
@@ -50,7 +51,7 @@ public:
             mFreeHandles.emplace_back(h);
         } else {
             lock.unlock();
-            CloseHandle(h); // don't collect too many open events
+            CloseHandle(h);  // don't collect too many open events
         }
     }
 
@@ -61,43 +62,170 @@ private:
 
 LazyInstance<WaitEventStorage> sWaitEvents = LAZY_INSTANCE_INIT;
 
-}  // namespace
+////////////////////////////////////////////////////////////////////////////////
+// An interface for the OS-specific condition variable implementation.
+class ConditionVariableForOs {
+public:
+    virtual void initCV(ConditionVariable::Data* cvData) const = 0;
+    virtual void waitCV(ConditionVariable::Data* cvData, Lock* lock) const = 0;
+    virtual void signalCV(ConditionVariable::Data* cvData) const = 0;
+    virtual void destroyCV(ConditionVariable::Data* cvData) const = 0;
 
-ConditionVariable::~ConditionVariable() {
-    // Don't lock anything here: if there's someone blocked on |this| while
-    // we're in a dtor in a different thread, the code is already heavily
-    // bugged. Lock here won't save it.
-}
+protected:
+    ~ConditionVariableForOs() noexcept = default;
+};
 
-void ConditionVariable::wait(Lock* userLock) {
-    // Grab new waiter event handle.
-    HANDLE handle = sWaitEvents->alloc();
-    {
-        AutoLock lock(mLock);
-        mWaiters.emplace_back(handle);
+// Fallback code for pre-Vista OS version - use handcrafted code.
+class ConditionVariableForXp final : public ConditionVariableForOs {
+public:
+    virtual void initCV(ConditionVariable::Data* cvData) const override final {
+        new (&cvData->xp) ConditionVariable::XpCV();
     }
+    virtual void waitCV(ConditionVariable::Data* cvData,
+                        Lock* userLock) const override final {
+        // Grab new waiter event handle.
+        HANDLE handle = sWaitEvents->alloc();
+        {
+            AutoLock lock(cvData->xp.mLock);
+            cvData->xp.mWaiters.emplace_back(handle);
+        }
 
-    // Unlock user lock then wait for event.
-    userLock->unlock();
-    WaitForSingleObject(handle, INFINITE);
-    // NOTE: The handle has been removed from mWaiters here,
-    // see signal() below. Close/recycle the event.
-    sWaitEvents->free(handle);
-    userLock->lock();
-}
-
-void ConditionVariable::signal() {
-    android::base::AutoLock lock(mLock);
-    if (!mWaiters.empty()) {
+        // Unlock user lock then wait for event.
+        userLock->unlock();
+        WaitForSingleObject(handle, INFINITE);
+        // NOTE: The handle has been removed from mWaiters here,
+        // see signal() below. Close/recycle the event.
+        sWaitEvents->free(handle);
+        userLock->lock();
+    }
+    virtual void signalCV(
+            ConditionVariable::Data* cvData) const override final {
+        android::base::AutoLock lock(cvData->xp.mLock);
+        if (cvData->xp.mWaiters.empty()) {
+            return;
+        }
         // NOTE: This wakes up the thread that went to sleep most
         //       recently (LIFO) for performance reason. For better
         //       fairness, using (FIFO) would be appropriate.
-        HANDLE handle = mWaiters.back().release();
-        mWaiters.pop_back();
+        HANDLE handle = cvData->xp.mWaiters.back().release();
+        cvData->xp.mWaiters.pop_back();
         lock.unlock();
         SetEvent(handle);
         // NOTE: The handle will be closed/recycled by the waiter.
     }
+    virtual void destroyCV(
+            ConditionVariable::Data* cvData) const override final {
+        cvData->xp.ConditionVariable::XpCV::~XpCV();
+    }
+};
+
+// A set of Vista+ APIs for condition variables: function types and pointers.
+using InitCV = void(WINAPI*)(ConditionVariable::VistaCV*);
+using SleepCsCV = BOOL(WINAPI*)(ConditionVariable::VistaCV*,
+                                CRITICAL_SECTION*,
+                                DWORD);
+using SleepSrwLockCV = BOOL(WINAPI*)(ConditionVariable::VistaCV*,
+                                     void*,
+                                     DWORD,
+                                     ULONG);
+using WakeCV = void(WINAPI*)(ConditionVariable::VistaCV*);
+
+static InitCV sInitCV = nullptr;
+static SleepCsCV sSleepCsCV = nullptr;
+static SleepSrwLockCV sSleepSrwLockCV = nullptr;
+static WakeCV sWakeAllCV = nullptr;
+static WakeCV sWakeOneCV = nullptr;
+
+// Vista+ code for the condition variable, using the raw Windows API.
+class ConditionVariableForVista final : public ConditionVariableForOs {
+public:
+    virtual void initCV(ConditionVariable::Data* cvData) const override final {
+        sInitCV(&cvData->vista);
+    }
+    virtual void waitCV(ConditionVariable::Data* cvData,
+                        Lock* userLock) const override final {
+        sSleepCsCV(&cvData->vista, &userLock->mLock, INFINITE);
+    }
+    virtual void signalCV(
+            ConditionVariable::Data* cvData) const override final {
+        sWakeOneCV(&cvData->vista);
+    }
+    virtual void destroyCV(
+            ConditionVariable::Data* cvData) const override final {
+        ;  // there's no special function for condition variable destruction
+    }
+};
+
+// Instances of both implementations and a pointer that will point to the one
+// to use on the current OS.
+const ConditionVariableForXp sImplXp;
+const ConditionVariableForVista sImplVista;
+const ConditionVariableForOs* sImpl = nullptr;
+
+// A very simple class with the purpose of initializing the |sImpl| into
+// the proper implementation based on a dynamic function loading result.
+class ApiVersionSelector final {
+public:
+    ApiVersionSelector() {
+        static const wchar_t kDllName[] = L"kernel32.dll";
+
+        HMODULE kernel32 = ::GetModuleHandleW(kDllName);
+        if (!kernel32) {
+            kernel32 = ::LoadLibraryW(kDllName);
+            if (!kernel32) {
+                sImpl = &sImplXp;
+                return;  // well, this is a very strange breed of Windows...
+            }
+        }
+
+        const auto init = (InitCV)::GetProcAddress(
+                kernel32, "InitializeConditionVariable");
+        const auto sleepCs = (SleepCsCV)::GetProcAddress(
+                kernel32, "SleepConditionVariableCS");
+        const auto sleepSrw = (SleepSrwLockCV)::GetProcAddress(
+                kernel32, "SleepConditionVariableSRW");
+        const auto wakeOne =
+                (WakeCV)::GetProcAddress(kernel32, "WakeConditionVariable");
+        const auto wakeAll =
+                (WakeCV)::GetProcAddress(kernel32, "WakeAllConditionVariable");
+        if (!init || !sleepCs || !sleepSrw || !wakeOne || !wakeAll) {
+            sImpl = &sImplXp;
+            return;
+        }
+
+        sInitCV = init;
+        sSleepCsCV = sleepCs;
+        sSleepSrwLockCV = sleepSrw;
+        sWakeOneCV = wakeOne;
+        sWakeAllCV = wakeAll;
+        sImpl = &sImplVista;
+    }
+};
+
+LazyInstance<ApiVersionSelector> sApiLoader = LAZY_INSTANCE_INIT;
+
+}  // namespace
+
+// Actual ConditionVariable members - these just forward all calls to |sImpl|.
+
+ConditionVariable::ConditionVariable() {
+    if (!sImpl) {
+        sApiLoader.get();
+    }
+    assert(sImpl);
+    sImpl->initCV(&mData);
+}
+
+ConditionVariable::~ConditionVariable() {
+    sImpl->destroyCV(&mData);
+}
+
+void ConditionVariable::wait(Lock* userLock) {
+    sImpl->waitCV(&mData, userLock);
+}
+
+void ConditionVariable::signal() {
+    sImpl->signalCV(&mData);
 }
 
 }  // namespace base
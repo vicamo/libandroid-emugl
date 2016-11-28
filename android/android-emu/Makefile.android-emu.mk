aosp-mirror/platform_external_qemu

[video] goldfish_dma androidemu changes
This set of CLs instantiates the goldfish dma
pipe extension's interface, and uses it to
perform all color buffer updates, making
video playback smoother.

Change-Id: I543ba907fcc948d48d812146fa2e69a86cecb941

Makefile.android-emu.mk
@@ -252,6 +252,7 @@ LOCAL_SRC_FILES := \
     android/emulation/control/ScreenCapturer.cpp \
     android/emulation/CpuAccelerator.cpp \
     android/emulation/GoldfishSyncCommandQueue.cpp \
+    android/emulation/goldfish_dma.cpp \
     android/emulation/goldfish_sync.cpp \
     android/emulation/nand_limits.c \
     android/emulation/ParameterList.cpp \

vicamo/libandroid-emugl

android/android-emu/Makefile.android-emu.mk
@@ -267,6 +267,8 @@ LOCAL_SRC_FILES := \
     android/emulation/control/LineConsumer.cpp \
     android/emulation/control/ScreenCapturer.cpp \
     android/emulation/CpuAccelerator.cpp \
+    android/emulation/DmaMap.cpp \
+    android/emulation/GoldfishDma.cpp \
     android/emulation/GoldfishSyncCommandQueue.cpp \
     android/emulation/goldfish_sync.cpp \
     android/emulation/nand_limits.c \
@@ -522,6 +524,7 @@ LOCAL_SRC_FILES := \
   android/emulation/bufprint_config_dirs_unittest.cpp \
   android/emulation/ConfigDirs_unittest.cpp \
   android/emulation/DeviceContextRunner_unittest.cpp \
+  android/emulation/DmaMap_unittest.cpp \
   android/emulation/control/AdbInterface_unittest.cpp \
   android/emulation/control/ApkInstaller_unittest.cpp \
   android/emulation/control/FilePusher_unittest.cpp \
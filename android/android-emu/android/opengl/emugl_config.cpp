aosp-mirror/platform_external_qemu

Interface to set GLES backend from UI
1. UI gets a new dropdown box to set the GLES renderer to
use next boot:

- Auto (same as -gpu auto) (default)
- ANGLE/ANGLE9: -gpu angle11 / -gpu angle9 (angle11 is named "ANGLE"
  because it falls back to DX9 if not suppported)
- SwiftShader: -gpu swiftshader (on host)
- Desktop native OpenGL: -gpu host

2. The -gpu command line option overrides the UI setting, and the UI
setting overrides the AVD config.

3. Stop doing the extra emugl config in the beginning. Instead,
add all existing backend search paths unconditionally from the launcher.
Custom backend users may have to add a LD_LIBRARY_PATH argument.

Change-Id: I6ccd5fdab7bce5fc96baf4c5315eae362de00216

android/android-emu/android/opengl/emugl_config.cpp
@@ -101,6 +101,10 @@ SelectedRenderer emuglConfig_get_renderer(const char* gpu_mode) {
         return SELECTED_RENDERER_SWIFTSHADER;
     } else if (!strcmp(gpu_mode, "angle")) {
         return SELECTED_RENDERER_ANGLE;
+    } else if (!strcmp(gpu_mode, "angle11")) {
+        return SELECTED_RENDERER_ANGLE11;
+    } else if (!strcmp(gpu_mode, "angle9")) {
+        return SELECTED_RENDERER_ANGLE9;
     } else if (!strcmp(gpu_mode, "error")) {
         return SELECTED_RENDERER_ERROR;
     } else {
@@ -120,6 +124,17 @@ void free_emugl_host_gpu_props(emugl_host_gpu_prop_list proplist) {
     delete [] proplist.props;
 }
 
+// Should match android/android-emu/android/skin/qt/qt-settings.h's
+// GLESBACKEND_PREFERENCE_VALUE
+
+enum UIPreferredBackend {
+    UIPREFERREDBACKEND_AUTO = 0,
+    UIPREFERREDBACKEND_ANGLE11 = 1,
+    UIPREFERREDBACKEND_ANGLE9 = 2,
+    UIPREFERREDBACKEND_SWIFTSHADER = 3,
+    UIPREFERREDBACKEND_NATIVEGL = 4,
+};
+
 bool emuglConfig_init(EmuglConfig* config,
                       bool gpu_enabled,
                       const char* gpu_mode,
@@ -127,7 +142,8 @@ bool emuglConfig_init(EmuglConfig* config,
                       int bitness,
                       bool no_window,
                       bool blacklisted,
-                      bool has_guest_renderer) {
+                      bool has_guest_renderer,
+                      int uiPreferredBackend) {
     D("%s: blacklisted=%d has_guest_renderer=%d\n",
       __FUNCTION__,
       blacklisted,
@@ -136,8 +152,10 @@ bool emuglConfig_init(EmuglConfig* config,
     // zero all fields first.
     memset(config, 0, sizeof(*config));
 
-    // The value of '-gpu <mode>' overrides the hardware properties,
-    // except if <mode> is 'auto'.
+    bool hasUiPreference = uiPreferredBackend != UIPREFERREDBACKEND_AUTO;
+
+    // The value of '-gpu <mode>' overrides both the hardware properties
+    // and the UI setting, except if <mode> is 'auto'.
     if (gpu_option) {
         if (!strcmp(gpu_option, "on") || !strcmp(gpu_option, "enable")) {
             gpu_enabled = true;
@@ -170,6 +188,11 @@ bool emuglConfig_init(EmuglConfig* config,
         gpu_enabled = false;
     }
 
+    if (!gpu_option && hasUiPreference) {
+        gpu_enabled = true;
+        gpu_mode = "auto";
+    }
+
     if (!gpu_enabled) {
         config->enabled = false;
         snprintf(config->backend, sizeof(config->backend), "%s", gpu_mode);
@@ -181,6 +204,7 @@ bool emuglConfig_init(EmuglConfig* config,
     if (!bitness) {
         bitness = System::get()->getProgramBitness();
     }
+
     config->bitness = bitness;
     resetBackendList(bitness);
 
@@ -206,7 +230,7 @@ bool emuglConfig_init(EmuglConfig* config,
             }
             D("%s: 'swiftshader' mode auto-selected\n", __FUNCTION__);
             gpu_mode = "swiftshader";
-        } else if (no_window || blacklisted) {
+        } else if (no_window || (blacklisted && !hasUiPreference)) {
             if (stringVectorContains(sBackendList->names(), "swiftshader")) {
                 D("%s: Headless (-no-window) mode (or blacklisted GPU driver)"
                   ", using Swiftshader backend\n",
@@ -235,8 +259,25 @@ bool emuglConfig_init(EmuglConfig* config,
                 return true;
             }
         } else {
-            D("%s: 'host' mode auto-selected\n", __FUNCTION__);
-            gpu_mode = "host";
+            switch (uiPreferredBackend) {
+                case UIPREFERREDBACKEND_ANGLE11:
+                    gpu_mode = "angle11";
+                    break;
+                case UIPREFERREDBACKEND_ANGLE9:
+                    gpu_mode = "angle9";
+                    break;
+                case UIPREFERREDBACKEND_SWIFTSHADER:
+                    gpu_mode = "swiftshader";
+                    break;
+                case UIPREFERREDBACKEND_NATIVEGL:
+                    gpu_mode = "host";
+                    break;
+                default:
+                    gpu_mode = "host";
+                    break;
+            }
+            D("%s: auto-selected %s based on conditions and UI preference %d\n",
+              __func__, gpu_mode, uiPreferredBackend);
         }
     }
 

android/android-emu/android/skin/qt/extended-pages/settings-page.cpp
@@ -101,6 +101,36 @@ SettingsPage::SettingsPage(QWidget* parent)
         default:
             break;
     }
+
+    Ui::Settings::GLESBACKEND_PREFERENCE_VALUE glesbackend_pref =
+        static_cast<Ui::Settings::GLESBACKEND_PREFERENCE_VALUE>(
+            settings.value(Ui::Settings::GLESBACKEND_PREFERENCE, 0).toInt());
+
+    switch (glesbackend_pref) {
+    case Ui::Settings::GLESBACKEND_PREFERENCE_AUTO:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_AUTO);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE11:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE11);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE9:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE9);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_SWIFTSHADER:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_SWIFTSHADER);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_NATIVEGL:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_NATIVEGL);
+        break;
+    default:
+        break;
+    }
+
 }
 
 void SettingsPage::setAdbInterface(android::emulation::AdbInterface* adb) {
@@ -278,5 +308,25 @@ void SettingsPage::on_set_crashReportPrefComboBox_currentIndexChanged(int index)
         set_reportPref_to(Ui::Settings::CRASHREPORT_PREFERENCE_NEVER);
     } else if (index == Ui::Settings::CRASHREPORT_COMBOBOX_ASK) {
         set_reportPref_to(Ui::Settings::CRASHREPORT_PREFERENCE_ASK);
+    }
+}
+
+static void set_glesBackend_to(Ui::Settings::GLESBACKEND_PREFERENCE_VALUE v) {
+    QSettings settings;
+    settings.setValue(Ui::Settings::GLESBACKEND_PREFERENCE, v);
+}
+
+void SettingsPage::on_set_glesBackendPrefComboBox_currentIndexChanged(int index) {
+    switch (index) {
+    case Ui::Settings::GLESBACKEND_PREFERENCE_AUTO:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE11:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE9:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_SWIFTSHADER:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_NATIVEGL:
+        fprintf(stderr, "%s: set to %d\n", __func__, index);
+        set_glesBackend_to((Ui::Settings::GLESBACKEND_PREFERENCE_VALUE)index);
+        break;
+    default:
+        break;
     }
 }

android/android-emu/android/skin/qt/winsys-qt.cpp
@@ -25,6 +25,7 @@
 #include "android/skin/qt/emulator-qt-window.h"
 #include "android/skin/qt/emulator-qt-no-window.h"
 #include "android/skin/qt/init-qt.h"
+#include "android/skin/qt/qt-settings.h"
 #include "android/utils/setenv.h"
 #include "android/main-common-ui.h"
 
@@ -233,6 +234,13 @@ extern bool skin_winsys_is_window_fully_visible()
     semaphore.acquire();
     D("%s: result = %s", __FUNCTION__, value ? "true" : "false");
     return value;
+}
+
+extern int skin_winsys_get_preferred_gles_backend()
+{
+    D("skin_winsys_is_window_fully_visible");
+    QSettings settings;
+    return settings.value(Ui::Settings::GLESBACKEND_PREFERENCE, 0).toInt();
 }
 
 extern void skin_winsys_quit_request()

vicamo/libandroid-emugl

android/android-emu/android/opengl/emugl_config.cpp
@@ -101,6 +101,8 @@ SelectedRenderer emuglConfig_get_renderer(const char* gpu_mode) {
         return SELECTED_RENDERER_SWIFTSHADER;
     } else if (!strcmp(gpu_mode, "angle")) {
         return SELECTED_RENDERER_ANGLE;
+    } else if (!strcmp(gpu_mode, "angle9")) {
+        return SELECTED_RENDERER_ANGLE9;
     } else if (!strcmp(gpu_mode, "error")) {
         return SELECTED_RENDERER_ERROR;
     } else {
@@ -120,6 +122,17 @@ void free_emugl_host_gpu_props(emugl_host_gpu_prop_list proplist) {
     delete [] proplist.props;
 }
 
+// Should match android/android-emu/android/skin/qt/qt-settings.h's
+// GLESBACKEND_PREFERENCE_VALUE
+
+enum UIPreferredBackend {
+    UIPREFERREDBACKEND_AUTO = 0,
+    UIPREFERREDBACKEND_ANGLE = 1,
+    UIPREFERREDBACKEND_ANGLE9 = 2,
+    UIPREFERREDBACKEND_SWIFTSHADER = 3,
+    UIPREFERREDBACKEND_NATIVEGL = 4,
+};
+
 bool emuglConfig_init(EmuglConfig* config,
                       bool gpu_enabled,
                       const char* gpu_mode,
@@ -127,7 +140,8 @@ bool emuglConfig_init(EmuglConfig* config,
                       int bitness,
                       bool no_window,
                       bool blacklisted,
-                      bool has_guest_renderer) {
+                      bool has_guest_renderer,
+                      int uiPreferredBackend) {
     D("%s: blacklisted=%d has_guest_renderer=%d\n",
       __FUNCTION__,
       blacklisted,
@@ -136,8 +150,10 @@ bool emuglConfig_init(EmuglConfig* config,
     // zero all fields first.
     memset(config, 0, sizeof(*config));
 
-    // The value of '-gpu <mode>' overrides the hardware properties,
-    // except if <mode> is 'auto'.
+    bool hasUiPreference = uiPreferredBackend != UIPREFERREDBACKEND_AUTO;
+
+    // The value of '-gpu <mode>' overrides both the hardware properties
+    // and the UI setting, except if <mode> is 'auto'.
     if (gpu_option) {
         if (!strcmp(gpu_option, "on") || !strcmp(gpu_option, "enable")) {
             gpu_enabled = true;
@@ -170,6 +186,11 @@ bool emuglConfig_init(EmuglConfig* config,
         gpu_enabled = false;
     }
 
+    if (!gpu_option && hasUiPreference) {
+        gpu_enabled = true;
+        gpu_mode = "auto";
+    }
+
     if (!gpu_enabled) {
         config->enabled = false;
         snprintf(config->backend, sizeof(config->backend), "%s", gpu_mode);
@@ -181,6 +202,7 @@ bool emuglConfig_init(EmuglConfig* config,
     if (!bitness) {
         bitness = System::get()->getProgramBitness();
     }
+
     config->bitness = bitness;
     resetBackendList(bitness);
 
@@ -206,7 +228,7 @@ bool emuglConfig_init(EmuglConfig* config,
             }
             D("%s: 'swiftshader' mode auto-selected\n", __FUNCTION__);
             gpu_mode = "swiftshader";
-        } else if (no_window || blacklisted) {
+        } else if (no_window || (blacklisted && !hasUiPreference)) {
             if (stringVectorContains(sBackendList->names(), "swiftshader")) {
                 D("%s: Headless (-no-window) mode (or blacklisted GPU driver)"
                   ", using Swiftshader backend\n",
@@ -235,8 +257,25 @@ bool emuglConfig_init(EmuglConfig* config,
                 return true;
             }
         } else {
-            D("%s: 'host' mode auto-selected\n", __FUNCTION__);
-            gpu_mode = "host";
+            switch (uiPreferredBackend) {
+                case UIPREFERREDBACKEND_ANGLE:
+                    gpu_mode = "angle";
+                    break;
+                case UIPREFERREDBACKEND_ANGLE9:
+                    gpu_mode = "angle9";
+                    break;
+                case UIPREFERREDBACKEND_SWIFTSHADER:
+                    gpu_mode = "swiftshader";
+                    break;
+                case UIPREFERREDBACKEND_NATIVEGL:
+                    gpu_mode = "host";
+                    break;
+                default:
+                    gpu_mode = "host";
+                    break;
+            }
+            D("%s: auto-selected %s based on conditions and UI preference %d\n",
+              __func__, gpu_mode, uiPreferredBackend);
         }
     }
 

android/android-emu/android/skin/qt/extended-pages/settings-page.cpp
@@ -101,6 +101,36 @@ SettingsPage::SettingsPage(QWidget* parent)
         default:
             break;
     }
+
+    Ui::Settings::GLESBACKEND_PREFERENCE_VALUE glesbackend_pref =
+        static_cast<Ui::Settings::GLESBACKEND_PREFERENCE_VALUE>(
+            settings.value(Ui::Settings::GLESBACKEND_PREFERENCE, 0).toInt());
+
+    switch (glesbackend_pref) {
+    case Ui::Settings::GLESBACKEND_PREFERENCE_AUTO:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_AUTO);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE9:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE9);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_SWIFTSHADER:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_SWIFTSHADER);
+        break;
+    case Ui::Settings::GLESBACKEND_PREFERENCE_NATIVEGL:
+        mUi->set_glesBackendPrefComboBox->setCurrentIndex(
+                Ui::Settings::GLESBACKEND_PREFERENCE_NATIVEGL);
+        break;
+    default:
+        break;
+    }
+
 }
 
 void SettingsPage::setAdbInterface(android::emulation::AdbInterface* adb) {
@@ -278,5 +308,24 @@ void SettingsPage::on_set_crashReportPrefComboBox_currentIndexChanged(int index)
         set_reportPref_to(Ui::Settings::CRASHREPORT_PREFERENCE_NEVER);
     } else if (index == Ui::Settings::CRASHREPORT_COMBOBOX_ASK) {
         set_reportPref_to(Ui::Settings::CRASHREPORT_PREFERENCE_ASK);
+    }
+}
+
+static void set_glesBackend_to(Ui::Settings::GLESBACKEND_PREFERENCE_VALUE v) {
+    QSettings settings;
+    settings.setValue(Ui::Settings::GLESBACKEND_PREFERENCE, v);
+}
+
+void SettingsPage::on_set_glesBackendPrefComboBox_currentIndexChanged(int index) {
+    switch (index) {
+    case Ui::Settings::GLESBACKEND_PREFERENCE_AUTO:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_ANGLE9:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_SWIFTSHADER:
+    case Ui::Settings::GLESBACKEND_PREFERENCE_NATIVEGL:
+        set_glesBackend_to((Ui::Settings::GLESBACKEND_PREFERENCE_VALUE)index);
+        break;
+    default:
+        break;
     }
 }

android/android-emu/android/skin/qt/winsys-qt.cpp
@@ -25,6 +25,7 @@
 #include "android/skin/qt/emulator-qt-window.h"
 #include "android/skin/qt/emulator-qt-no-window.h"
 #include "android/skin/qt/init-qt.h"
+#include "android/skin/qt/qt-settings.h"
 #include "android/utils/setenv.h"
 #include "android/main-common-ui.h"
 
@@ -233,6 +234,13 @@ extern bool skin_winsys_is_window_fully_visible()
     semaphore.acquire();
     D("%s: result = %s", __FUNCTION__, value ? "true" : "false");
     return value;
+}
+
+extern int skin_winsys_get_preferred_gles_backend()
+{
+    D("skin_winsys_get_preferred_gles_backend");
+    QSettings settings;
+    return settings.value(Ui::Settings::GLESBACKEND_PREFERENCE, 0).toInt();
 }
 
 extern void skin_winsys_quit_request()
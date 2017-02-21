aosp-mirror/platform_external_qemu

Bug Report Page UI Forms
Keep the previous hehavior of the bug report button and
activate the bug report page after everything is ready.

Change-Id: Ic3bb2a9f62fea75ff0c11fc50616b870c12f5428
Signed-off-by: Weilun Du <wdu@google.com>

android/android-emu/android/skin/qt/sources.mk
@@ -52,6 +52,7 @@ ANDROID_SKIN_SOURCES += \
     android/skin/qt/extended-pages/common.cpp \
     android/skin/qt/extended-pages/battery-page.cpp \
     android/skin/qt/extended-pages/car-data-page.cpp \
+    android/skin/qt/extended-pages/bug-report-page.cpp \
     android/skin/qt/extended-pages/cellular-page.cpp \
     android/skin/qt/extended-pages/dpad-page.cpp \
     android/skin/qt/extended-pages/finger-page.cpp \
@@ -87,6 +88,7 @@ ANDROID_SKIN_QT_MOC_SRC_FILES := \
     android/skin/qt/event-subscriber.h \
     android/skin/qt/extended-pages/battery-page.h \
     android/skin/qt/extended-pages/car-data-page.h \
+    android/skin/qt/extended-pages/bug-report-page.h \
     android/skin/qt/extended-pages/cellular-page.h \
     android/skin/qt/extended-pages/dpad-page.h \
     android/skin/qt/extended-pages/finger-page.h \
@@ -117,6 +119,7 @@ ANDROID_SKIN_QT_UI_SRC_FILES := \
     android/skin/qt/extended.ui \
     android/skin/qt/extended-pages/car-data-page.ui \
     android/skin/qt/extended-pages/battery-page.ui \
+    android/skin/qt/extended-pages/bug-report-page.ui \
     android/skin/qt/extended-pages/cellular-page.ui \
     android/skin/qt/extended-pages/dpad-page.ui \
     android/skin/qt/extended-pages/finger-page.ui \

vicamo/libandroid-emugl

android/android-emu/android/skin/qt/sources.mk
@@ -52,6 +52,7 @@ ANDROID_SKIN_SOURCES += \
     android/skin/qt/extended-pages/common.cpp \
     android/skin/qt/extended-pages/battery-page.cpp \
     android/skin/qt/extended-pages/car-data-page.cpp \
+    android/skin/qt/extended-pages/bug-report-window.cpp \
     android/skin/qt/extended-pages/cellular-page.cpp \
     android/skin/qt/extended-pages/dpad-page.cpp \
     android/skin/qt/extended-pages/finger-page.cpp \
@@ -88,6 +89,7 @@ ANDROID_SKIN_QT_MOC_SRC_FILES := \
     android/skin/qt/event-subscriber.h \
     android/skin/qt/extended-pages/battery-page.h \
     android/skin/qt/extended-pages/car-data-page.h \
+    android/skin/qt/extended-pages/bug-report-window.h \
     android/skin/qt/extended-pages/cellular-page.h \
     android/skin/qt/extended-pages/dpad-page.h \
     android/skin/qt/extended-pages/finger-page.h \
@@ -118,6 +120,7 @@ ANDROID_SKIN_QT_UI_SRC_FILES := \
     android/skin/qt/extended.ui \
     android/skin/qt/extended-pages/car-data-page.ui \
     android/skin/qt/extended-pages/battery-page.ui \
+    android/skin/qt/extended-pages/bug-report-window.ui \
     android/skin/qt/extended-pages/cellular-page.ui \
     android/skin/qt/extended-pages/dpad-page.ui \
     android/skin/qt/extended-pages/finger-page.ui \
aosp-mirror/platform_external_qemu

[UI] Implement Play Store and Play Services Button.
Change-Id: Ibbd6034f82c8ddb854a08a88f5516c6aba60307e

android/android-emu/android/emulation/control/GooglePlayServices.cpp
@@ -1,0 +1,80 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/emulation/control/GooglePlayServices.h"
+
+namespace android {
+namespace emulation {
+
+using android::base::System;
+
+// static
+const System::Duration GooglePlayServices::kTimeoutMs = 5000;
+
+GooglePlayServices::~GooglePlayServices() {
+    if (mStoreSettingsCommand) {
+        mStoreSettingsCommand->cancel();
+    }
+    if (mServicesPageCommand) {
+        mServicesPageCommand->cancel();
+    }
+}
+
+void GooglePlayServices::setAdbInterface(AdbInterface* adb) {
+    mAdb = adb;
+}
+
+void GooglePlayServices::showPlayStoreSettings(ResultCallback resultCallback) {
+    if (!mAdb) {
+        return;
+    }
+
+    if (mStoreSettingsCommand) {
+        resultCallback(Result::kOperationInProgress);
+    }
+    mStoreSettingsCommand = mAdb->runAdbCommand(
+            {"shell", "am start", "-n",
+             "com.android.vending/"
+             "com.google.android.finsky.activities.SettingsActivity"},
+            [this, resultCallback](const OptionalAdbCommandResult& result) {
+                resultCallback((!result || result->exit_code)
+                                       ? Result::kUnknownError
+                                       : Result::kSuccess);
+                mStoreSettingsCommand.reset();
+            },
+            kTimeoutMs, false);
+}
+
+void GooglePlayServices::showPlayServicesPage(ResultCallback resultCallback) {
+    if (!mAdb) {
+        return;
+    }
+
+    if (mServicesPageCommand) {
+        resultCallback(Result::kOperationInProgress);
+    }
+    mServicesPageCommand = mAdb->runAdbCommand(
+            {"shell", "am start", "-a", "android.intent.action.VIEW", "-d",
+             "'market://details?id=com.google.android.gms'"},
+            [this, resultCallback](const OptionalAdbCommandResult& result) {
+                resultCallback((!result || result->exit_code)
+                                       ? Result::kUnknownError
+                                       : Result::kSuccess);
+                mServicesPageCommand.reset();
+            },
+            kTimeoutMs, false);
+}
+
+}  // namespace emulation
+}  // namespace android

android/android-emu/android/emulation/control/GooglePlayServices.h
@@ -1,0 +1,55 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/base/Compiler.h"
+#include "android/base/system/System.h"
+#include "android/emulation/control/AdbInterface.h"
+
+#include <functional>
+
+namespace android {
+namespace emulation {
+
+class GooglePlayServices {
+public:
+    enum class Result { kSuccess, kOperationInProgress, kUnknownError };
+
+    using ResultCallback = std::function<void(Result result)>;
+
+    explicit GooglePlayServices(AdbInterface* adb) : mAdb(adb) {}
+    ~GooglePlayServices();
+
+    void setAdbInterface(AdbInterface* adb);
+    // Opens the Google play store settings in the play store app.
+    void showPlayStoreSettings(ResultCallback resultCallback);
+    // Opens the Play Services app details page in the play store.
+    void showPlayServicesPage(ResultCallback resultCallback);
+
+private:
+    static const android::base::System::Duration kTimeoutMs;
+
+    AdbInterface* mAdb;
+    AdbCommandPtr mStoreSettingsCommand;
+    AdbCommandPtr mServicesPageCommand;
+    const ResultCallback mResultCallback;
+
+    unsigned mNonDefaultCheckTimeoutForTestMs = 0;
+
+    DISALLOW_COPY_AND_ASSIGN(GooglePlayServices);
+};
+
+}  // namespace emulation
+}  // namespace android

vicamo/libandroid-emugl

android/android-emu/android/emulation/control/GooglePlayServices.cpp
@@ -1,0 +1,78 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "android/emulation/control/GooglePlayServices.h"
+
+namespace android {
+namespace emulation {
+
+using android::base::System;
+
+// static
+const System::Duration GooglePlayServices::kAdbCommandTimeoutMs = 5000;
+
+GooglePlayServices::~GooglePlayServices() {
+    if (mStoreSettingsCommand) {
+        mStoreSettingsCommand->cancel();
+    }
+    if (mServicesPageCommand) {
+        mServicesPageCommand->cancel();
+    }
+}
+
+void GooglePlayServices::showPlayStoreSettings(ResultCallback resultCallback) {
+    if (!mAdb) {
+        return;
+    }
+
+    if (mStoreSettingsCommand) {
+        resultCallback(Result::OperationInProgress);
+        return;
+    }
+    mStoreSettingsCommand = mAdb->runAdbCommand(
+            {"shell", "am start", "-n",
+             "com.android.vending/"
+             "com.google.android.finsky.activities.SettingsActivity"},
+            [this, resultCallback](const OptionalAdbCommandResult& result) {
+                resultCallback((!result || result->exit_code)
+                                       ? Result::UnknownError
+                                       : Result::Success);
+                mStoreSettingsCommand.reset();
+            },
+            kAdbCommandTimeoutMs, false);
+}
+
+void GooglePlayServices::showPlayServicesPage(ResultCallback resultCallback) {
+    if (!mAdb) {
+        return;
+    }
+
+    if (mServicesPageCommand) {
+        resultCallback(Result::OperationInProgress);
+        return;
+    }
+    mServicesPageCommand = mAdb->runAdbCommand(
+            {"shell", "am start", "-a", "android.intent.action.VIEW", "-d",
+             "'market://details?id=com.google.android.gms'"},
+            [this, resultCallback](const OptionalAdbCommandResult& result) {
+                resultCallback((!result || result->exit_code)
+                                       ? Result::UnknownError
+                                       : Result::Success);
+                mServicesPageCommand.reset();
+            },
+            kAdbCommandTimeoutMs, false);
+}
+
+}  // namespace emulation
+}  // namespace android

android/android-emu/android/emulation/control/GooglePlayServices.h
@@ -1,0 +1,52 @@
+// Copyright (C) 2017 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include "android/base/Compiler.h"
+#include "android/base/system/System.h"
+#include "android/emulation/control/AdbInterface.h"
+
+#include <functional>
+
+namespace android {
+namespace emulation {
+
+class GooglePlayServices {
+public:
+    enum class Result { Success, OperationInProgress, UnknownError };
+
+    using ResultCallback = std::function<void(Result result)>;
+
+    explicit GooglePlayServices(AdbInterface* adb) : mAdb(adb) {}
+    ~GooglePlayServices();
+
+    // Opens the Google play store settings in the play store app.
+    void showPlayStoreSettings(ResultCallback resultCallback);
+    // Opens the Play Services app details page in the play store.
+    void showPlayServicesPage(ResultCallback resultCallback);
+
+private:
+    static const android::base::System::Duration kAdbCommandTimeoutMs;
+
+    AdbInterface* mAdb;
+    AdbCommandPtr mStoreSettingsCommand;
+    AdbCommandPtr mServicesPageCommand;
+    const ResultCallback mResultCallback;
+
+    DISALLOW_COPY_AND_ASSIGN(GooglePlayServices);
+};
+
+}  // namespace emulation
+}  // namespace android
aosp-mirror/platform_external_qemu

[GPU snapshot] Restore program to GPU
This patch restore program states to GPU.

Change-Id: I31ce3b2ec8efda6aa28b27ebd81a773ad5e1fd61

android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
@@ -2790,74 +2790,119 @@ GL_APICALL void  GL_APIENTRY glTexSubImage2D(GLenum target, GLint level, GLint x
     ctx->dispatcher().glTexSubImage2D(target,level,xoffset,yoffset,width,height,format,type,pixels);
 }
 
+static void setUniformmv(GLint location, int count, GLboolean transpose, GLenum type,
+        size_t typeSize, const void* data) {
+    GET_CTX_V2();
+    ProgramData* programData = ctx->getUseProgram();
+    if (programData) {
+        size_t size = count * typeSize;
+        GLUniformDesc uniformDesc(location, count, transpose, type, size,
+                (unsigned char*)data);
+        programData->addUniform(location, std::move(uniformDesc));
+    }
+}
+
 GL_APICALL void  GL_APIENTRY glUniform1f(GLint location, GLfloat x){
     GET_CTX();
+    GLfloat v[] = {x};
+    setUniformmv(location, 1, false, GL_FLOAT, sizeof(GLfloat), v);
     ctx->dispatcher().glUniform1f(location,x);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform1fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_FLOAT, sizeof(GLfloat), v);
     ctx->dispatcher().glUniform1fv(location,count,v);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform1i(GLint location, GLint x){
     GET_CTX();
+    GLint v[] = {x};
+    setUniformmv(location, 1, false, GL_INT, sizeof(GLint), v);
     ctx->dispatcher().glUniform1i(location,x);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform1iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_INT, sizeof(GLint), v);
     ctx->dispatcher().glUniform1iv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2f(GLint location, GLfloat x, GLfloat y){
     GET_CTX();
+    GLfloat v[] = {x, y};
+    setUniformmv(location, 1, false, GL_FLOAT_VEC2, sizeof(GLfloat) * 2, v);
     ctx->dispatcher().glUniform2f(location,x,y);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_FLOAT_VEC2, sizeof(GLfloat) * 2, v);
     ctx->dispatcher().glUniform2fv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2i(GLint location, GLint x, GLint y){
     GET_CTX();
+    GLint v[] = {x, y};
+    setUniformmv(location, 1, false, GL_INT_VEC2, sizeof(GLint) * 2, v);
     ctx->dispatcher().glUniform2i(location,x,y);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_INT_VEC2, sizeof(GLint) * 2, v);
     ctx->dispatcher().glUniform2iv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform3f(GLint location, GLfloat x, GLfloat y, GLfloat z){
     GET_CTX();
+    GLfloat v[] = {x, y, z};
+    setUniformmv(location, 1, false, GL_FLOAT_VEC3, sizeof(GLfloat) * 3, v);
     ctx->dispatcher().glUniform3f(location,x,y,z);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform3fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_FLOAT_VEC3, sizeof(GLfloat) * 3, v);
     ctx->dispatcher().glUniform3fv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform3i(GLint location, GLint x, GLint y, GLint z){
     GET_CTX();
+    GLint v[] = {x, y, z};
+    setUniformmv(location, 1, false, GL_INT_VEC3, sizeof(GLint) * 3, v);
     ctx->dispatcher().glUniform3i(location,x,y,z);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform3iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_INT_VEC3, sizeof(GLint) * 3, v);
     ctx->dispatcher().glUniform3iv(location,count,v);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4f(GLint location, GLfloat x, GLfloat y, GLfloat z, GLfloat w){
     GET_CTX();
+    GLfloat v[] = {x, y, z, w};
+    setUniformmv(location, 1, false, GL_FLOAT_VEC4, sizeof(GLfloat) * 4, v);
     ctx->dispatcher().glUniform4f(location,x,y,z,w);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_FLOAT_VEC4, sizeof(GLfloat) * 4, v);
     ctx->dispatcher().glUniform4fv(location,count,v);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4i(GLint location, GLint x, GLint y, GLint z, GLint w){
     GET_CTX();
+    GLint v[] = {x, y, z, w};
+    setUniformmv(location, 1, false, GL_INT_VEC4, sizeof(GLint) * 4, v);
     ctx->dispatcher().glUniform4i(location,x,y,z,w);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformmv(location, count, false, GL_INT_VEC4, sizeof(GLint) * 4, v);
     ctx->dispatcher().glUniform4iv(location,count,v);
 }
 
@@ -2865,6 +2910,7 @@ GL_APICALL void  GL_APIENTRY glUniformMatrix2fv(GLint location, GLsizei count, G
     GET_CTX_V2();
     SET_ERROR_IF(ctx->getMajorVersion() < 3 &&
                  transpose != GL_FALSE,GL_INVALID_VALUE);
+    setUniformmv(location, count, transpose, GL_FLOAT_MAT2, sizeof(GLfloat) * 4, value);
     ctx->dispatcher().glUniformMatrix2fv(location,count,transpose,value);
 }
 
@@ -2872,6 +2918,7 @@ GL_APICALL void  GL_APIENTRY glUniformMatrix3fv(GLint location, GLsizei count, G
     GET_CTX_V2();
     SET_ERROR_IF(ctx->getMajorVersion() < 3 &&
                  transpose != GL_FALSE,GL_INVALID_VALUE);
+    setUniformmv(location, count, transpose, GL_FLOAT_MAT3, sizeof(GLfloat) * 9, value);
     ctx->dispatcher().glUniformMatrix3fv(location,count,transpose,value);
 }
 
@@ -2879,6 +2926,7 @@ GL_APICALL void  GL_APIENTRY glUniformMatrix4fv(GLint location, GLsizei count, G
     GET_CTX_V2();
     SET_ERROR_IF(ctx->getMajorVersion() < 3 &&
                  transpose != GL_FALSE,GL_INVALID_VALUE);
+    setUniformmv(location, count, transpose, GL_FLOAT_MAT4, sizeof(GLfloat) * 16, value);
     ctx->dispatcher().glUniformMatrix4fv(location,count,transpose,value);
 }
 

vicamo/libandroid-emugl

android/android-emugl/host/libs/Translator/GLES_V2/GLESv2Imp.cpp
@@ -1352,7 +1352,11 @@ GL_APICALL int GL_APIENTRY glGetAttribLocation(GLuint program, const GLchar* nam
          ProgramData* pData = (ProgramData*)objData;
          RET_AND_SET_ERROR_IF(pData->getLinkStatus() != GL_TRUE,
                               GL_INVALID_OPERATION, -1);
-         return ctx->dispatcher().glGetAttribLocation(globalProgramName, name);
+         int ret = ctx->dispatcher().glGetAttribLocation(globalProgramName, name);
+         if (ret != -1) {
+             pData->linkedAttribLocation(name, ret);
+         }
+         return ret;
      }
      return -1;
 }
@@ -2790,74 +2794,119 @@ GL_APICALL void  GL_APIENTRY glTexSubImage2D(GLenum target, GLint level, GLint x
     ctx->dispatcher().glTexSubImage2D(target,level,xoffset,yoffset,width,height,format,type,pixels);
 }
 
+static void setUniformInCtx(GLint location, int count, GLboolean transpose, GLenum type,
+        size_t typeSize, const void* data) {
+    GET_CTX_V2();
+    ProgramData* programData = ctx->getUseProgram();
+    if (programData) {
+        size_t size = count * typeSize;
+        GLUniformDesc uniformDesc(location, count, transpose, type, size,
+                (unsigned char*)data);
+        programData->addUniform(location, std::move(uniformDesc));
+    }
+}
+
 GL_APICALL void  GL_APIENTRY glUniform1f(GLint location, GLfloat x){
     GET_CTX();
+    GLfloat v[] = {x};
+    setUniformInCtx(location, 1, false, GL_FLOAT, sizeof(GLfloat), v);
     ctx->dispatcher().glUniform1f(location,x);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform1fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_FLOAT, sizeof(GLfloat), v);
     ctx->dispatcher().glUniform1fv(location,count,v);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform1i(GLint location, GLint x){
     GET_CTX();
+    GLint v[] = {x};
+    setUniformInCtx(location, 1, false, GL_INT, sizeof(GLint), v);
     ctx->dispatcher().glUniform1i(location,x);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform1iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_INT, sizeof(GLint), v);
     ctx->dispatcher().glUniform1iv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2f(GLint location, GLfloat x, GLfloat y){
     GET_CTX();
+    GLfloat v[] = {x, y};
+    setUniformInCtx(location, 1, false, GL_FLOAT_VEC2, sizeof(GLfloat) * 2, v);
     ctx->dispatcher().glUniform2f(location,x,y);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_FLOAT_VEC2, sizeof(GLfloat) * 2, v);
     ctx->dispatcher().glUniform2fv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2i(GLint location, GLint x, GLint y){
     GET_CTX();
+    GLint v[] = {x, y};
+    setUniformInCtx(location, 1, false, GL_INT_VEC2, sizeof(GLint) * 2, v);
     ctx->dispatcher().glUniform2i(location,x,y);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform2iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_INT_VEC2, sizeof(GLint) * 2, v);
     ctx->dispatcher().glUniform2iv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform3f(GLint location, GLfloat x, GLfloat y, GLfloat z){
     GET_CTX();
+    GLfloat v[] = {x, y, z};
+    setUniformInCtx(location, 1, false, GL_FLOAT_VEC3, sizeof(GLfloat) * 3, v);
     ctx->dispatcher().glUniform3f(location,x,y,z);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform3fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_FLOAT_VEC3, sizeof(GLfloat) * 3, v);
     ctx->dispatcher().glUniform3fv(location,count,v);
 }
+
 GL_APICALL void  GL_APIENTRY glUniform3i(GLint location, GLint x, GLint y, GLint z){
     GET_CTX();
+    GLint v[] = {x, y, z};
+    setUniformInCtx(location, 1, false, GL_INT_VEC3, sizeof(GLint) * 3, v);
     ctx->dispatcher().glUniform3i(location,x,y,z);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform3iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_INT_VEC3, sizeof(GLint) * 3, v);
     ctx->dispatcher().glUniform3iv(location,count,v);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4f(GLint location, GLfloat x, GLfloat y, GLfloat z, GLfloat w){
     GET_CTX();
+    GLfloat v[] = {x, y, z, w};
+    setUniformInCtx(location, 1, false, GL_FLOAT_VEC4, sizeof(GLfloat) * 4, v);
     ctx->dispatcher().glUniform4f(location,x,y,z,w);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4fv(GLint location, GLsizei count, const GLfloat* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_FLOAT_VEC4, sizeof(GLfloat) * 4, v);
     ctx->dispatcher().glUniform4fv(location,count,v);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4i(GLint location, GLint x, GLint y, GLint z, GLint w){
     GET_CTX();
+    GLint v[] = {x, y, z, w};
+    setUniformInCtx(location, 1, false, GL_INT_VEC4, sizeof(GLint) * 4, v);
     ctx->dispatcher().glUniform4i(location,x,y,z,w);
 }
 
 GL_APICALL void  GL_APIENTRY glUniform4iv(GLint location, GLsizei count, const GLint* v){
     GET_CTX();
+    setUniformInCtx(location, count, false, GL_INT_VEC4, sizeof(GLint) * 4, v);
     ctx->dispatcher().glUniform4iv(location,count,v);
 }
 
@@ -2865,6 +2914,7 @@ GL_APICALL void  GL_APIENTRY glUniformMatrix2fv(GLint location, GLsizei count, G
     GET_CTX_V2();
     SET_ERROR_IF(ctx->getMajorVersion() < 3 &&
                  transpose != GL_FALSE,GL_INVALID_VALUE);
+    setUniformInCtx(location, count, transpose, GL_FLOAT_MAT2, sizeof(GLfloat) * 4, value);
     ctx->dispatcher().glUniformMatrix2fv(location,count,transpose,value);
 }
 
@@ -2872,6 +2922,7 @@ GL_APICALL void  GL_APIENTRY glUniformMatrix3fv(GLint location, GLsizei count, G
     GET_CTX_V2();
     SET_ERROR_IF(ctx->getMajorVersion() < 3 &&
                  transpose != GL_FALSE,GL_INVALID_VALUE);
+    setUniformInCtx(location, count, transpose, GL_FLOAT_MAT3, sizeof(GLfloat) * 9, value);
     ctx->dispatcher().glUniformMatrix3fv(location,count,transpose,value);
 }
 
@@ -2879,6 +2930,7 @@ GL_APICALL void  GL_APIENTRY glUniformMatrix4fv(GLint location, GLsizei count, G
     GET_CTX_V2();
     SET_ERROR_IF(ctx->getMajorVersion() < 3 &&
                  transpose != GL_FALSE,GL_INVALID_VALUE);
+    setUniformInCtx(location, count, transpose, GL_FLOAT_MAT4, sizeof(GLfloat) * 16, value);
     ctx->dispatcher().glUniformMatrix4fv(location,count,transpose,value);
 }
 
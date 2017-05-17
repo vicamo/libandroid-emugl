aosp-mirror/platform_external_qemu

Vendor image support
This CL mount vendor image if there is one.

Change-Id: Ice42c7b511d6ddb5225f6272e147b21bc62a2063

android-qemu2-glue/main.cpp
@@ -97,9 +97,11 @@ enum ImageType {
     IMAGE_TYPE_USER_DATA,
     IMAGE_TYPE_SD_CARD,
     IMAGE_TYPE_ENCRYPTION_KEY,
+    IMAGE_TYPE_VENDOR,
+    IMAGE_TYPE_MAX
 };
 
-const int kMaxPartitions = 5;
+const int kMaxPartitions = IMAGE_TYPE_MAX;
 const int kMaxTargetQemuParams = 16;
 
 /*
@@ -117,7 +119,7 @@ const int kMaxTargetQemuParams = 16;
  * listed in command line is mounted first, i.e. to /dev/block/vda,
  * the next one to /dev/block/vdb, etc. However, for arm/mips, it's reversed;
  * the last one is mounted to /dev/block/vda. the 2nd last to /dev/block/vdb.
- * So far, we have 4(kMaxPartitions) types defined for system, cache, userdata
+ * So far, we have 6(kMaxPartitions) types defined for system, cache, userdata
  * and sdcard images.
  * |qemuExtraArgs| are the qemu parameters specific to the target platform.
  * this is a NULL-terminated list of string pointers of at most
@@ -143,7 +145,7 @@ const TargetInfo kTarget = {
     "ttyAMA",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_VENDOR, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_ARM)
     "arm",
@@ -152,7 +154,7 @@ const TargetInfo kTarget = {
     "ttyAMA",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_VENDOR, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_MIPS64)
     "mips64",
@@ -161,7 +163,7 @@ const TargetInfo kTarget = {
     "ttyGF",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_VENDOR, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_MIPS)
     "mips",
@@ -170,7 +172,7 @@ const TargetInfo kTarget = {
     "ttyGF",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_VENDOR, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_X86_64)
     "x86_64",
@@ -179,7 +181,7 @@ const TargetInfo kTarget = {
     "ttyS",
     "virtio-blk-pci",
     "virtio-net-pci",
-    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD},
+    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR},
     {"-vga", "none", NULL},
 #elif defined(TARGET_I386)  // Both i386 and x86_64 targets define this macro
     "x86",
@@ -188,7 +190,7 @@ const TargetInfo kTarget = {
     "ttyS",
     "virtio-blk-pci",
     "virtio-net-pci",
-    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD},
+    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR},
     {"-vga", "none", NULL},
 #else
     #error No target platform is defined
@@ -249,6 +251,17 @@ static void makePartitionCmd(const char** args, int* argsPosition, int* driveInd
                     driveParam += ",read-only";
             }
             deviceParam = StringFormat("%s,drive=system",
+                                       kTarget.storageDeviceType);
+            break;
+        case IMAGE_TYPE_VENDOR:
+            filePath = avdInfo_getContentPath(avd);
+            driveParam += StringFormat("index=%d,id=vendor,file=%s"
+                                       PATH_SEP "vendor.img.qcow2",
+                                        idx++, filePath);
+            // You can override this explicitly
+            // by passing -writable-system to emulator.
+            if (!writable) driveParam += ",read-only";
+            deviceParam = StringFormat("%s,drive=vendor",
                                        kTarget.storageDeviceType);
             break;
         case IMAGE_TYPE_CACHE:
@@ -864,7 +877,8 @@ extern "C" int main(int argc, char **argv) {
     int s;
     int drvIndex = 0;
     for (s = 0; s < kMaxPartitions; s++) {
-        bool writable = (kTarget.imagePartitionTypes[s] == IMAGE_TYPE_SYSTEM) ?
+        bool writable = (kTarget.imagePartitionTypes[s] == IMAGE_TYPE_SYSTEM
+                    || kTarget.imagePartitionTypes[s] == IMAGE_TYPE_VENDOR) ?
                     android_op_writable_system : true;
         makePartitionCmd(args, &n, &drvIndex, hw,
                          kTarget.imagePartitionTypes[s], writable, apiLevel, avd);

android/android-emu/android/main-common.c
@@ -425,6 +425,13 @@ static AvdInfo* createAVD(AndroidOptions* opts, int* inAndroidBuild) {
             D("autoconfig: -system %s", opts->system);
         }
 
+        if (!opts->vendor) {
+            str_reset_nocopy(
+                    &opts->vendor,
+                    _getSdkSystemImage(opts->sysdir, "-vendor", "vendor.img"));
+            D("autoconfig: -vendor %s", opts->system);
+        }
+
         if (!opts->kernel) {
             str_reset_nocopy(
                     &opts->kernel,
@@ -812,6 +819,81 @@ static bool emulator_handleCommonEmulatorOptions(AndroidOptions* opts,
         hw->disk_systemPartition_size =
             _adjustPartitionSize("system", systemBytes, defaultPartitionSize,
                                  avdInfo_inAndroidBuild(avd));
+    }
+
+    /** VENDOR PARTITION **/
+    {
+        char*  rwImage   = NULL;
+        char*  initImage = NULL;
+
+        do {
+            if (opts->vendor == NULL) {
+                /* If -vendor is not used, try to find a runtime system image
+                * (i.e. vendor-qemu.img) in the content directory.
+                */
+                rwImage = avdInfo_getVendorImagePath(avd);
+                if (rwImage != NULL) {
+                    break;
+                }
+                /* Otherwise, try to find the initial system image */
+                initImage = avdInfo_getVendorInitImagePath(avd);
+                /* Vendor image is for O+ only */
+                break;
+            }
+
+            /* If -vendor <name> is used, use it to find the initial image */
+            if (opts->sysdir != NULL && !path_exists(opts->vendor)) {
+                initImage = _getFullFilePath(opts->sysdir, opts->vendor);
+            } else {
+                initImage = ASTRDUP(opts->system);
+            }
+            /* Vendor image is for O+ only */
+            if (!path_exists(initImage)) {
+                initImage = NULL;
+            }
+
+        } while (0);
+
+        if (rwImage != NULL) {
+            /* Use the read/write image file directly */
+            str_reset_nocopy(&hw->disk_vendorPartition_path, rwImage);
+            str_reset_null(&hw->disk_vendorPartition_initPath);
+            D("Using direct vendor image: %s", rwImage);
+        } else if (initImage != NULL) {
+            str_reset_null(&hw->disk_vendorPartition_path);
+            str_reset_nocopy(&hw->disk_vendorPartition_initPath, initImage);
+            D("Using initial vendor image: %s", initImage);
+        } else {
+            str_reset_null(&hw->disk_vendorPartition_path);
+            str_reset_null(&hw->disk_vendorPartition_initPath);
+            D("No vendor image");
+        }
+
+        /* Check the size of the vendor partition image.
+        * If we have an AVD, it must be smaller than
+        * the disk.systemPartition.size hardware property.
+        *
+        * Otherwise, we need to adjust the systemPartitionSize
+        * automatically, and print a warning.
+        *
+        */
+        const char* vendorImage = hw->disk_vendorPartition_path;
+        uint64_t    vendorBytes = 0;
+
+        if (vendorImage == NULL) {
+            vendorImage = hw->disk_vendorPartition_initPath;
+        }
+
+        if (vendorImage) {
+            if (path_get_size(vendorImage, &vendorBytes) < 0) {
+                derror("Missing vendor image: %s", vendorImage);
+                return false;
+            }
+
+            hw->disk_vendorPartition_size =
+                _adjustPartitionSize("system", vendorBytes, defaultPartitionSize,
+                                     avdInfo_inAndroidBuild(avd));
+        }
     }
 
     /** DATA PARTITION **/

vicamo/libandroid-emugl

BUG: 37511975

android-qemu2-glue/main.cpp
@@ -96,9 +96,11 @@ enum ImageType {
     IMAGE_TYPE_USER_DATA,
     IMAGE_TYPE_SD_CARD,
     IMAGE_TYPE_ENCRYPTION_KEY,
+    IMAGE_TYPE_VENDOR,
+    IMAGE_TYPE_MAX
 };
 
-const int kMaxPartitions = 5;
+const int kMaxPartitions = IMAGE_TYPE_MAX;
 const int kMaxTargetQemuParams = 16;
 
 /*
@@ -116,7 +118,7 @@ const int kMaxTargetQemuParams = 16;
  * listed in command line is mounted first, i.e. to /dev/block/vda,
  * the next one to /dev/block/vdb, etc. However, for arm/mips, it's reversed;
  * the last one is mounted to /dev/block/vda. the 2nd last to /dev/block/vdb.
- * So far, we have 4(kMaxPartitions) types defined for system, cache, userdata
+ * So far, we have 6(kMaxPartitions) types defined for system, cache, userdata
  * and sdcard images.
  * |qemuExtraArgs| are the qemu parameters specific to the target platform.
  * this is a NULL-terminated list of string pointers of at most
@@ -142,7 +144,7 @@ const TargetInfo kTarget = {
     "ttyAMA",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_ARM)
     "arm",
@@ -151,7 +153,7 @@ const TargetInfo kTarget = {
     "ttyAMA",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_MIPS64)
     "mips64",
@@ -160,7 +162,7 @@ const TargetInfo kTarget = {
     "ttyGF",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_MIPS)
     "mips",
@@ -169,7 +171,7 @@ const TargetInfo kTarget = {
     "ttyGF",
     "virtio-blk-device",
     "virtio-net-device",
-    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
+    {IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_CACHE, IMAGE_TYPE_SYSTEM},
     {NULL},
 #elif defined(TARGET_X86_64)
     "x86_64",
@@ -178,7 +180,7 @@ const TargetInfo kTarget = {
     "ttyS",
     "virtio-blk-pci",
     "virtio-net-pci",
-    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD},
+    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD, IMAGE_TYPE_VENDOR},
     {"-vga", "none", NULL},
 #elif defined(TARGET_I386)  // Both i386 and x86_64 targets define this macro
     "x86",
@@ -187,7 +189,7 @@ const TargetInfo kTarget = {
     "ttyS",
     "virtio-blk-pci",
     "virtio-net-pci",
-    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_SD_CARD},
+    {IMAGE_TYPE_SYSTEM, IMAGE_TYPE_CACHE, IMAGE_TYPE_USER_DATA, IMAGE_TYPE_ENCRYPTION_KEY, IMAGE_TYPE_VENDOR, IMAGE_TYPE_SD_CARD},
     {"-vga", "none", NULL},
 #else
     #error No target platform is defined
@@ -252,6 +254,22 @@ static void makePartitionCmd(const char** args, int* argsPosition, int* driveInd
             }
             allocatedPath.reset(filePath.c_str());
             deviceParam = StringFormat("%s,drive=system",
+                                       kTarget.storageDeviceType);
+            break;
+        case IMAGE_TYPE_VENDOR:
+            if (!hw->disk_vendorPartition_path
+                    && !hw->disk_vendorPartition_initPath) {
+                // we do not have a vendor image to mount
+                return;
+            }
+            filePath = avdInfo_getContentPath(avd);
+            driveParam += StringFormat("index=%d,id=vendor,file=%s"
+                                       PATH_SEP "vendor.img.qcow2",
+                                        idx++, filePath);
+            // You can override this explicitly
+            // by passing -writable-system to emulator.
+            if (!writable) driveParam += ",read-only";
+            deviceParam = StringFormat("%s,drive=vendor",
                                        kTarget.storageDeviceType);
             break;
         case IMAGE_TYPE_CACHE:
@@ -901,7 +919,8 @@ extern "C" int main(int argc, char **argv) {
     int s;
     int drvIndex = 0;
     for (s = 0; s < kMaxPartitions; s++) {
-        bool writable = (kTarget.imagePartitionTypes[s] == IMAGE_TYPE_SYSTEM) ?
+        bool writable = (kTarget.imagePartitionTypes[s] == IMAGE_TYPE_SYSTEM
+                    || kTarget.imagePartitionTypes[s] == IMAGE_TYPE_VENDOR) ?
                     android_op_writable_system : true;
         makePartitionCmd(args, &n, &drvIndex, hw,
                          kTarget.imagePartitionTypes[s], writable, apiLevel, avd);

android/android-emu/android/main-common.c
@@ -425,6 +425,13 @@ static AvdInfo* createAVD(AndroidOptions* opts, int* inAndroidBuild) {
             D("autoconfig: -system %s", opts->system);
         }
 
+        if (!opts->vendor) {
+            str_reset_nocopy(
+                    &opts->vendor,
+                    _getSdkSystemImage(opts->sysdir, "-vendor", "vendor.img"));
+            D("autoconfig: -vendor %s", opts->vendor);
+        }
+
         if (!opts->kernel) {
             str_reset_nocopy(
                     &opts->kernel,
@@ -812,6 +819,81 @@ static bool emulator_handleCommonEmulatorOptions(AndroidOptions* opts,
         hw->disk_systemPartition_size =
             _adjustPartitionSize("system", systemBytes, defaultPartitionSize,
                                  avdInfo_inAndroidBuild(avd));
+    }
+
+    /** VENDOR PARTITION **/
+    {
+        char*  rwImage   = NULL;
+        char*  initImage = NULL;
+
+        do {
+            if (opts->vendor == NULL) {
+                /* If -vendor is not used, try to find a runtime system image
+                * (i.e. vendor-qemu.img) in the content directory.
+                */
+                rwImage = avdInfo_getVendorImagePath(avd);
+                if (rwImage != NULL) {
+                    break;
+                }
+                /* Otherwise, try to find the initial system image */
+                initImage = avdInfo_getVendorInitImagePath(avd);
+                /* Vendor image is for O+ only */
+                break;
+            }
+
+            /* If -vendor <name> is used, use it to find the initial image */
+            if (opts->sysdir != NULL && !path_exists(opts->vendor)) {
+                initImage = _getFullFilePath(opts->sysdir, opts->vendor);
+            } else {
+                initImage = ASTRDUP(opts->vendor);
+            }
+            /* Vendor image is for O+ only */
+            if (!path_exists(initImage)) {
+                initImage = NULL;
+            }
+
+        } while (0);
+
+        if (rwImage != NULL) {
+            /* Use the read/write image file directly */
+            str_reset_nocopy(&hw->disk_vendorPartition_path, rwImage);
+            str_reset_null(&hw->disk_vendorPartition_initPath);
+            D("Using direct vendor image: %s", rwImage);
+        } else if (initImage != NULL) {
+            str_reset_null(&hw->disk_vendorPartition_path);
+            str_reset_nocopy(&hw->disk_vendorPartition_initPath, initImage);
+            D("Using initial vendor image: %s", initImage);
+        } else {
+            str_reset_null(&hw->disk_vendorPartition_path);
+            str_reset_null(&hw->disk_vendorPartition_initPath);
+            D("No vendor image");
+        }
+
+        /* Check the size of the vendor partition image.
+        * If we have an AVD, it must be smaller than
+        * the disk.systemPartition.size hardware property.
+        *
+        * Otherwise, we need to adjust the systemPartitionSize
+        * automatically, and print a warning.
+        *
+        */
+        const char* vendorImage = hw->disk_vendorPartition_path;
+        uint64_t    vendorBytes = 0;
+
+        if (vendorImage == NULL) {
+            vendorImage = hw->disk_vendorPartition_initPath;
+        }
+
+        if (vendorImage) {
+            if (path_get_size(vendorImage, &vendorBytes) < 0) {
+                derror("Missing vendor image: %s", vendorImage);
+                return false;
+            }
+
+            hw->disk_vendorPartition_size =
+                _adjustPartitionSize("vendor", vendorBytes, defaultPartitionSize,
+                                     avdInfo_inAndroidBuild(avd));
+        }
     }
 
     /** DATA PARTITION **/
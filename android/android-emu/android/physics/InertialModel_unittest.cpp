aosp-mirror/platform_external_qemu

Add support for retrieving current and target parameters.
This adds an argument passed through to the physical model to allow callers to
retrieve the target or current position as needed, along with related tests.

Change-Id: I20fcee3a80f5c177416f576f5bc0493e4573f6c6

android/android-emu/android/physics/InertialModel_unittest.cpp
@@ -162,7 +162,7 @@ TEST(InertialModel, SmoothAcceleration) {
 
     glm::vec3 singleIntegratedPosition = glm::vec3(0.f);
 
-    constexpr uint64_t step = 500UL;
+    constexpr uint64_t step = 5000UL;
     constexpr float timeIncrement = step / 1000000000.f;
     for (uint64_t time = step >> 1; time < 500000000; time += step) {
         inertialModel.setCurrentTime(time);
@@ -196,7 +196,7 @@ TEST(InertialModel, MultipleTargets) {
 
     glm::vec3 singleIntegratedPosition = glm::vec3(0.f);
 
-    constexpr uint64_t step = 500UL;
+    constexpr uint64_t step = 5000UL;
     constexpr float timeIncrement = step / 1000000000.f;
     uint64_t time = step >> 1;
     for (; time < 250000000; time += step) {
@@ -225,4 +225,91 @@ TEST(InertialModel, MultipleTargets) {
     EXPECT_NEAR(singleIntegratedPosition.x, targetPosition.x, 0.05f);
     EXPECT_NEAR(singleIntegratedPosition.y, targetPosition.y, 0.05f);
     EXPECT_NEAR(singleIntegratedPosition.z, targetPosition.z, 0.05f);
+}
+
+
+TEST(InertialModel, TargetValues) {
+    TestSystem testSystem("/", System::kProgramBitness);
+
+    const glm::vec3 targetPosition (14.0f, -3.5f, 0.01f);
+    const glm::quat targetRotation (28.f, glm::vec3(0.f, -1.f, 0.f));
+
+    InertialModel inertialModel;
+    inertialModel.setCurrentTime(0UL);
+    inertialModel.setTargetPosition(glm::vec3(0.f), PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetRotation(glm::quat(), PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetPosition(targetPosition, PHYSICAL_INTERPOLATION_SMOOTH);
+    inertialModel.setTargetRotation(targetRotation, PHYSICAL_INTERPOLATION_SMOOTH);
+
+    const glm::vec3 retrievedTargetPosition =
+            inertialModel.getPosition(PARAMETER_VALUE_TARGET);
+    EXPECT_NEAR(targetPosition.x, retrievedTargetPosition.x, 0.0001f);
+    EXPECT_NEAR(targetPosition.y, retrievedTargetPosition.y, 0.0001f);
+    EXPECT_NEAR(targetPosition.z, retrievedTargetPosition.z, 0.0001f);
+
+    const glm::quat retrievedTargetRotation =
+            inertialModel.getRotation(PARAMETER_VALUE_TARGET);
+    EXPECT_NEAR(targetRotation.x, retrievedTargetRotation.x, 0.0001f);
+    EXPECT_NEAR(targetRotation.y, retrievedTargetRotation.y, 0.0001f);
+    EXPECT_NEAR(targetRotation.z, retrievedTargetRotation.z, 0.0001f);
+    EXPECT_NEAR(targetRotation.w, retrievedTargetRotation.w, 0.0001f);
+}
+
+TEST(InertialModel, CurrentInitialPositionValue) {
+    TestSystem testSystem("/", System::kProgramBitness);
+
+    const glm::vec3 initialPosition (0.1f, -0.4f, 0.6f);
+
+    InertialModel inertialModel;
+    inertialModel.setCurrentTime(0UL);
+    inertialModel.setTargetPosition(initialPosition, PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetPosition(glm::vec3(0.3f, 0.9f, -0.5f), PHYSICAL_INTERPOLATION_SMOOTH);
+
+    const glm::vec3 retrievedCurrentPosition =
+            inertialModel.getPosition(PARAMETER_VALUE_CURRENT);
+    EXPECT_NEAR(initialPosition.x, retrievedCurrentPosition.x, 0.0001f);
+    EXPECT_NEAR(initialPosition.y, retrievedCurrentPosition.y, 0.0001f);
+    EXPECT_NEAR(initialPosition.z, retrievedCurrentPosition.z, 0.0001f);
+}
+
+
+TEST(InertialModel, IntermediateValuesDuringInterpolation) {
+    TestSystem testSystem("/", System::kProgramBitness);
+
+    const glm::vec3 initialPosition (0.3f, 0.1f, -2.0f);
+    const glm::vec3 targetPosition (5.0f, -2.0f, 1.0f);
+
+    InertialModel inertialModel;
+    inertialModel.setCurrentTime(0UL);
+    inertialModel.setTargetPosition(initialPosition, PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetPosition(targetPosition, PHYSICAL_INTERPOLATION_SMOOTH);
+
+    glm::vec3 integratedVelocity = glm::vec3(0.f);
+    glm::vec3 doubleIntegratedPosition = initialPosition;
+
+    glm::vec3 singleIntegratedPosition = initialPosition;
+
+    constexpr uint64_t step = 50000UL;
+    constexpr float timeIncrement = step / 1000000000.f;
+    for (uint64_t time = step >> 1; time < 500000000; time += step) {
+        inertialModel.setCurrentTime(time);
+        integratedVelocity += timeIncrement * inertialModel.getAcceleration();
+        doubleIntegratedPosition += timeIncrement * integratedVelocity;
+
+        const glm::vec3 measuredVelocity = inertialModel.getVelocity();
+        EXPECT_NEAR(measuredVelocity.x, integratedVelocity.x, 0.01f);
+        EXPECT_NEAR(measuredVelocity.y, integratedVelocity.y, 0.01f);
+        EXPECT_NEAR(measuredVelocity.z, integratedVelocity.z, 0.01f);
+
+        singleIntegratedPosition += timeIncrement * inertialModel.getVelocity();
+
+        const glm::vec3 measuredPosition = inertialModel.getPosition();
+        EXPECT_NEAR(measuredPosition.x, doubleIntegratedPosition.x, 0.01f);
+        EXPECT_NEAR(measuredPosition.y, doubleIntegratedPosition.y, 0.01f);
+        EXPECT_NEAR(measuredPosition.z, doubleIntegratedPosition.z, 0.01f);
+
+        EXPECT_NEAR(measuredPosition.x, singleIntegratedPosition.x, 0.01f);
+        EXPECT_NEAR(measuredPosition.y, singleIntegratedPosition.y, 0.01f);
+        EXPECT_NEAR(measuredPosition.z, singleIntegratedPosition.z, 0.01f);
+    }
 }

vicamo/libandroid-emugl

android/android-emu/android/physics/InertialModel_unittest.cpp
@@ -162,7 +162,7 @@ TEST(InertialModel, SmoothAcceleration) {
 
     glm::vec3 singleIntegratedPosition = glm::vec3(0.f);
 
-    constexpr uint64_t step = 500UL;
+    constexpr uint64_t step = 5000UL;
     constexpr float timeIncrement = step / 1000000000.f;
     for (uint64_t time = step >> 1; time < 500000000; time += step) {
         inertialModel.setCurrentTime(time);
@@ -196,7 +196,7 @@ TEST(InertialModel, MultipleTargets) {
 
     glm::vec3 singleIntegratedPosition = glm::vec3(0.f);
 
-    constexpr uint64_t step = 500UL;
+    constexpr uint64_t step = 5000UL;
     constexpr float timeIncrement = step / 1000000000.f;
     uint64_t time = step >> 1;
     for (; time < 250000000; time += step) {
@@ -225,4 +225,91 @@ TEST(InertialModel, MultipleTargets) {
     EXPECT_NEAR(singleIntegratedPosition.x, targetPosition.x, 0.05f);
     EXPECT_NEAR(singleIntegratedPosition.y, targetPosition.y, 0.05f);
     EXPECT_NEAR(singleIntegratedPosition.z, targetPosition.z, 0.05f);
+}
+
+
+TEST(InertialModel, TargetValues) {
+    TestSystem testSystem("/", System::kProgramBitness);
+
+    const glm::vec3 targetPosition (14.0f, -3.5f, 0.01f);
+    const glm::quat targetRotation (28.f, glm::vec3(0.f, -1.f, 0.f));
+
+    InertialModel inertialModel;
+    inertialModel.setCurrentTime(0UL);
+    inertialModel.setTargetPosition(glm::vec3(0.f), PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetRotation(glm::quat(), PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetPosition(targetPosition, PHYSICAL_INTERPOLATION_SMOOTH);
+    inertialModel.setTargetRotation(targetRotation, PHYSICAL_INTERPOLATION_SMOOTH);
+
+    const glm::vec3 retrievedTargetPosition =
+            inertialModel.getPosition(PARAMETER_VALUE_TYPE_TARGET);
+    EXPECT_NEAR(targetPosition.x, retrievedTargetPosition.x, 0.0001f);
+    EXPECT_NEAR(targetPosition.y, retrievedTargetPosition.y, 0.0001f);
+    EXPECT_NEAR(targetPosition.z, retrievedTargetPosition.z, 0.0001f);
+
+    const glm::quat retrievedTargetRotation =
+            inertialModel.getRotation(PARAMETER_VALUE_TYPE_TARGET);
+    EXPECT_NEAR(targetRotation.x, retrievedTargetRotation.x, 0.0001f);
+    EXPECT_NEAR(targetRotation.y, retrievedTargetRotation.y, 0.0001f);
+    EXPECT_NEAR(targetRotation.z, retrievedTargetRotation.z, 0.0001f);
+    EXPECT_NEAR(targetRotation.w, retrievedTargetRotation.w, 0.0001f);
+}
+
+TEST(InertialModel, CurrentInitialPositionValue) {
+    TestSystem testSystem("/", System::kProgramBitness);
+
+    const glm::vec3 initialPosition (0.1f, -0.4f, 0.6f);
+
+    InertialModel inertialModel;
+    inertialModel.setCurrentTime(0UL);
+    inertialModel.setTargetPosition(initialPosition, PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetPosition(glm::vec3(0.3f, 0.9f, -0.5f), PHYSICAL_INTERPOLATION_SMOOTH);
+
+    const glm::vec3 retrievedCurrentPosition =
+            inertialModel.getPosition(PARAMETER_VALUE_TYPE_CURRENT);
+    EXPECT_NEAR(initialPosition.x, retrievedCurrentPosition.x, 0.0001f);
+    EXPECT_NEAR(initialPosition.y, retrievedCurrentPosition.y, 0.0001f);
+    EXPECT_NEAR(initialPosition.z, retrievedCurrentPosition.z, 0.0001f);
+}
+
+
+TEST(InertialModel, IntermediateValuesDuringInterpolation) {
+    TestSystem testSystem("/", System::kProgramBitness);
+
+    const glm::vec3 initialPosition (0.3f, 0.1f, -2.0f);
+    const glm::vec3 targetPosition (5.0f, -2.0f, 1.0f);
+
+    InertialModel inertialModel;
+    inertialModel.setCurrentTime(0UL);
+    inertialModel.setTargetPosition(initialPosition, PHYSICAL_INTERPOLATION_STEP);
+    inertialModel.setTargetPosition(targetPosition, PHYSICAL_INTERPOLATION_SMOOTH);
+
+    glm::vec3 integratedVelocity = glm::vec3(0.f);
+    glm::vec3 doubleIntegratedPosition = initialPosition;
+
+    glm::vec3 singleIntegratedPosition = initialPosition;
+
+    constexpr uint64_t step = 50000UL;
+    constexpr float timeIncrement = step / 1000000000.f;
+    for (uint64_t time = step >> 1; time < 500000000; time += step) {
+        inertialModel.setCurrentTime(time);
+        integratedVelocity += timeIncrement * inertialModel.getAcceleration();
+        doubleIntegratedPosition += timeIncrement * integratedVelocity;
+
+        const glm::vec3 measuredVelocity = inertialModel.getVelocity();
+        EXPECT_NEAR(measuredVelocity.x, integratedVelocity.x, 0.01f);
+        EXPECT_NEAR(measuredVelocity.y, integratedVelocity.y, 0.01f);
+        EXPECT_NEAR(measuredVelocity.z, integratedVelocity.z, 0.01f);
+
+        singleIntegratedPosition += timeIncrement * inertialModel.getVelocity();
+
+        const glm::vec3 measuredPosition = inertialModel.getPosition();
+        EXPECT_NEAR(measuredPosition.x, doubleIntegratedPosition.x, 0.01f);
+        EXPECT_NEAR(measuredPosition.y, doubleIntegratedPosition.y, 0.01f);
+        EXPECT_NEAR(measuredPosition.z, doubleIntegratedPosition.z, 0.01f);
+
+        EXPECT_NEAR(measuredPosition.x, singleIntegratedPosition.x, 0.01f);
+        EXPECT_NEAR(measuredPosition.y, singleIntegratedPosition.y, 0.01f);
+        EXPECT_NEAR(measuredPosition.z, singleIntegratedPosition.z, 0.01f);
+    }
 }
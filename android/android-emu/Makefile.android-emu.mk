aosp-mirror/platform_external_qemu

Refactor Virtual Scene Renderer to use RenderCommands
To apply feedback from architecture discussions, update the Virtual
Scene Renderer to have each SceneObject return a list of RenderCommands,
which are then executed by the Renderer, instead of performing
per-object rendering from within each SceneObject.

To support this, create the Material and Mesh classes, which serve as
opaque container classes containing GLES types.

Additionally, to validate this functionality, add support for rendering
.obj files that reference .mtl files (only diffuse textures supported),
and support .obj files that contain multiple surfaces. This will allow
us to ship the virtual scene in a single .obj file that references
multiple textures.

Change-Id: I1517cd35ffdb03e2e19af78dfce533a59abc81c4

android/android-emu/Makefile.android-emu.mk
@@ -478,6 +478,8 @@ LOCAL_SRC_FILES := \
     android/utils/sockets.c \
     android/utils/looper.cpp \
     android/virtualscene/Effect.cpp \
+    android/virtualscene/Material.cpp \
+    android/virtualscene/Mesh.cpp \
     android/virtualscene/Renderer.cpp \
     android/virtualscene/RenderTarget.cpp \
     android/virtualscene/SceneCamera.cpp \

vicamo/libandroid-emugl

android/android-emu/Makefile.android-emu.mk
@@ -477,13 +477,13 @@ LOCAL_SRC_FILES := \
     android/utils/socket_drainer.cpp \
     android/utils/sockets.c \
     android/utils/looper.cpp \
-    android/virtualscene/Effect.cpp \
     android/virtualscene/Renderer.cpp \
     android/virtualscene/RenderTarget.cpp \
+    android/virtualscene/Scene.cpp \
     android/virtualscene/SceneCamera.cpp \
     android/virtualscene/SceneObject.cpp \
-    android/virtualscene/Texture.cpp \
-    android/virtualscene/VirtualScene.cpp \
+    android/virtualscene/TextureUtils.cpp \
+    android/virtualscene/VirtualSceneManager.cpp \
     android/wear-agent/android_wear_agent.cpp \
     android/wear-agent/WearAgent.cpp \
     android/wear-agent/PairUpWearPhone.cpp \